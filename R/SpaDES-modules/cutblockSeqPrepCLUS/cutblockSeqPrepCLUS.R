f07ac8dc13d216089e5090e52de596da38d69111
Lochhead <KLOCHHEA@BCGOV>
2018-05-07 16:09:44 -0700

    Connection to postgres working. Added an open/close of the connection. To Do: query select blocks based on harvest year

--- R/SpaDES-modules/cutblockSeqPrepCLUS/cutblockSeqPrepCLUS.R
+++ R/SpaDES-modules/cutblockSeqPrepCLUS/cutblockSeqPrepCLUS.R
@@ -11,15 +11,17 @@ defineModule(sim, list(
   timeunit = "year",
   citation = list("citation.bib"),
   documentation = list("README.txt", "cutblockSeqPrepCLUS.Rmd"),
-  reqdPkgs = list(),
+  reqdPkgs = list("rpostgis", "sp"),
   parameters = rbind(
     #defineParameter("paramName", "paramClass", value, min, max, "parameter description"),
-    defineParameter("dbName", "character", 'postgres', NA, NA, "The name of the postgres dataabse"),
+    defineParameter("dbName", "character", "postgres", NA, NA, "The name of the postgres dataabse"),
     defineParameter("dbHost", "character", 'localhost', NA, NA, "The name of the postgres host"),
     defineParameter("dbPort", "character", '5432', NA, NA, "The name of the postgres port"),
     defineParameter("dbUser", "character", 'postgres', NA, NA, "The name of the postgres user"),
     defineParameter("dbPassword", "character", 'postgres', NA, NA, "The name of the postgres user password"),
     defineParameter("dbGeom", "character", 'geom', NA, NA, "The name of the postgres file geom column"),
+    defineParameter("startTime", "numeric", start(sim), NA, NA, desc = "Simulation time at which to start"),
+    defineParameter("endTime", "numeric", end(sim), NA, NA, desc = "Simulation time at which to end"),
     defineParameter(".plotInitialTime", "numeric", NA, NA, NA, "This describes the simulation time at which the first plot event should occur"),
     defineParameter(".plotInterval", "numeric", NA, NA, NA, "This describes the simulation time interval between plot events"),
     defineParameter(".saveInitialTime", "numeric", NA, NA, NA, "This describes the simulation time at which the first save event should occur"),
@@ -27,35 +29,36 @@ defineModule(sim, list(
     defineParameter(".useCache", "numeric", FALSE, NA, NA, "Should this entire module be run with caching activated? This is generally intended for data-type modules, where stochasticity and time are not relevant")
   ),
   inputObjects = bind_rows(
-    expectsInput("herds", "list", "list of herd boundaries to include in the analysis", sourceURL = NA),
+    expectsInput("herds", "list", "list of herd boundaries to include in the analysis", sourceURL = NA)
     #expectsInput(objectName = NA, objectClass = NA, desc = NA, sourceURL = NA)
   ),
   outputObjects = bind_rows(
     #createsOutput("objectName", "objectClass", "output object description", ...),
-    createsOutput(objectName = NA, objectClass = NA, desc = NA)
+    #createsOutput(objectName = NA, objectClass = NA, desc = NA)
   )
 ))
-
-## event types
-#   - type `init` is required for initialiazation
 
 doEvent.cutblockSeqPrepCLUS = function(sim, eventTime, eventType, debug = FALSE) {
   switch(
     eventType,
     init = {
-      ### establish a connection?
       sim <- sim$cutblockSeqPrepCLUSdbConnect(sim)
-
+      sim <- scheduleEvent(sim, P(sim)$endTime, "cutblockSeqPrepCLUS", "endConnect")
+      
+      sim <- sim$cutblockSeqPrepCLUSgetBoundaries(sim)
       # do stuff for this event
-      sim <- Init(sim)
+      #sim <- Init(sim)
 
       # schedule future event(s)
-      sim <- scheduleEvent(sim, P(sim)$.plotInitialTime, "cutblockSeqPrepCLUS", "plot")
-      sim <- scheduleEvent(sim, P(sim)$.saveInitialTime, "cutblockSeqPrepCLUS", "save")
+      #sim <- scheduleEvent(sim, P(sim)$.plotInitialTime, "cutblockSeqPrepCLUS", "plot")
+      #sim <- scheduleEvent(sim, P(sim)$.saveInitialTime, "cutblockSeqPrepCLUS", "save")
     },
     plot = {
     },
     save = {
+    },
+    endConnect = {
+      sim <- sim$cutblockSeqPrepCLUSdbDisconnect(sim)
     },
     warning(paste("Undefined event type: '", current(sim)[1, "eventType", with = FALSE],
                   "' in module '", current(sim)[1, "moduleName", with = FALSE], "'", sep = ""))
@@ -77,36 +80,31 @@ Plot <- function(sim) {
 }
 
 cutblockSeqPrepCLUSdbConnect <- function(sim) {
-  sim$conn <- tryCatch(
-    {
-      conn<-dbConnect("PostgreSQL",dbname=sim$dbname, host=sim$host ,port=sim$port ,user=sim$user ,password=sim$password)
-    },
-    error=function(cond) {
-      message(paste("Connection to pgsql was not valid:", conn ))
-      message(cond)
-      out = FALSE
-      return(NA)
-    }
-  )    
-  return(conn)
+  sim$conn<-dbConnect("PostgreSQL",dbname= P(sim)$dbName, host=P(sim)$dbHost, port=P(sim)$dbPort ,user=P(sim)$dbUser, password=P(sim)$dbPassword)
+  return(invisible(sim))
+
+}
+cutblockSeqPrepCLUSdbDisconnect <- function(sim) {
+  dbDisconnect(sim$conn)
+  return(invisible(sim))
+  
+}
+cutblockSeqPrepCLUSgetBoundaries <- function(sim) {
+  name=c("gisdata","gcbp_carib_polygon")
+  
+  boundaries<-pgGetGeom(sim$conn, name=name,  geom = P(sim)$dbGeom)
+  sim$boundaries<-subset(boundaries , herd_name == P(sim)$herds[[1]] | herd_name == P(sim)$herds[[2]] |  herd_name == P(sim)$herds[[3]] )
+  
+  plot(sim$boundaries)
+  
+  return(invisible(sim))
 }
 .inputObjects <- function(sim) {
-  # Any code written here will be run during the simInit for the purpose of creating
-  # any objects required by this module and identified in the inputObjects element of defineModule.
-  # This is useful if there is something required before simulation to produce the module
-  # object dependencies, including such things as downloading default datasets, e.g.,
-  # downloadData("LCC2005", modulePath(sim)).
-  # Nothing should be created here that does not create an named object in inputObjects.
-  # Any other initiation procedures should be put in "init" eventType of the doEvent function.
-  # Note: the module developer can use 'sim$.userSuppliedObjNames' in their function below to
-  # selectively skip unnecessary steps because the user has provided those inputObjects in the
-  # simInit call. e.g.,
+
   # if (!('defaultColor' %in% sim$.userSuppliedObjNames)) {
   #  sim$defaultColor <- 'red'
   # }
-  # ! ----- EDIT BELOW ----- ! #
 
-  # ! ----- STOP EDITING ----- ! #
   return(invisible(sim))
 }
 ### add additional events as needed by copy/pasting from above
