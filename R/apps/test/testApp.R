b2c1ef6a22dcc6cc4bddcda6e4c7db04533a971c
Lochhead <KLOCHHEA@BCGOV>
2018-07-03 12:45:31 -0700

    aw - climate change (tyler's) in

--- R/apps/test/testApp.R
+++ R/apps/test/testApp.R
@@ -41,24 +41,34 @@ password='clus'
 ##Get a connection to the postgreSQL server (local instance)
 conn<-dbConnect(dbDriver("PostgreSQL"), host=host, dbname = dbname, port=port ,user=user ,password=password)
 ##Data objects
-###Get shapefile
+#----------------
+#Get cariobu herd boundaries
 name=c("public","gcbp_carib_polygon")
 geom = "geom"
 herd_bound <- spTransform(pgGetGeom(conn, name=name,  geom = geom), CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
 ####Remove NA's
 herd_bound <- herd_bound[which(herd_bound@data$herd_name != "NA"), ]
+#name=c("public","20180627_uwr_caribou_no_harvest")
+#uwrNH <- spTransform(pgGetGeom(conn, name=name,  geom = geom), CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
+#----------------
 #Get climate data
-bec<-dbGetQuery(conn,"SELECT * FROM public.clim_plot_data_bec")
-ffd<-dbGetQuery(conn,"SELECT * FROM public.clim_plot_data_spring_frost_free_days")
-wpas<-dbGetQuery(conn,"SELECT * FROM public.clim_plot_data_winter_precip_as_snow")
-wtemp<-dbGetQuery(conn,"SELECT * FROM public.clim_plot_data_winter_temp")
+bec<-dbGetQuery(conn,"SELECT * FROM public.clime_bec")
+bec$year <- relevel(as.factor(bec$year), "Current")
+clime<-dbGetQuery(conn,"SELECT * FROM public.clim_plot_data")
 #Get climate rasters from TYLER's ANALYSIS
 boreal <- raster::stack(pgGetRast(conn, "clim_pred_boreal_1990"),pgGetRast(conn, "clim_pred_boreal_2010"),pgGetRast(conn, "clim_pred_boreal_2025"), pgGetRast(conn, "clim_pred_boreal_2055"),pgGetRast(conn, "clim_pred_boreal_2085"))
 #mountain <- raster::stack(pgGetRast(conn, "clim_pred_mountain_1990"),pgGetRast(conn, "clim_pred_mountain_2010"),pgGetRast(conn, "clim_pred_mountain_2025"), pgGetRast(conn, "clim_pred_mountain_2055"),pgGetRast(conn, "clim_pred_mountain_2085"))
 #northern <- raster::stack(pgGetRast(conn, "clim_pred_northern_1990"),pgGetRast(conn, "clim_pred_northern_2010"),pgGetRast(conn, "clim_pred_northern_2025"), pgGetRast(conn, "clim_pred_northern_2055"),pgGetRast(conn, "clim_pred_northern_2085"))
+#----------------
+#get cached cutblock summary
+cb_sumALL<-dbGetQuery(conn,"SELECT * FROM public.cb_sum")
+#----------------
+#get cached road summary
+#----------------
+#get cached fire summary
 dbDisconnect(conn)
-#-------------------------------------------------------------------------------------------------
 
+#-------------------------------------------------------------------------------------------------
 # Define UI
 ui <- fluidPage(theme = shinytheme("lumen"),  
                 titlePanel("CLUS: Scenario Tool"),
@@ -99,23 +109,23 @@ mainPanel(
                         tabPanel("Wildlife Habitat Area"))
                  ),
                 tabPanel("Disturbance", navlistPanel(
-                    "Human", 
+                    "Herd Boundary", 
+                        tabPanel("Fire"),
                         tabPanel("Cutblock", plotOutput(outputId = "distPlot", height = "300px")),
                         tabPanel("Road"),
                         tabPanel("Other Linear"),
                         tabPanel("Total"),
-                    "Natural",
-                        tabPanel("Fire"),
-                        tabPanel("Insects")
+                    "Drawn",
+                        tabPanel("Total")
                     )
                   ),
-                tabPanel("Climate", navlistPanel(
+                tabPanel("Climate Change", navlistPanel(
                     "BEC", 
-                        tabPanel("Proportion", plotOutput(outputId = "becEcoTypePlot", height = "300px"), plotOutput(outputId = "becHerdPlot", height = "300px")), 
+                        tabPanel("Proportion", plotOutput(outputId = "becPlot", height = "300px")), 
                     "Variables", 
-                        tabPanel("Frost Free Days", plotOutput(outputId = "ffdEcoTypePlot", height = "300px"), plotOutput(outputId = "ffdHerdPlot", height = "300px")), 
-                        tabPanel("Precipitation as Snow", plotOutput(outputId = "pasEcoTypePlot", height = "300px"), plotOutput(outputId = "pasHerdPlot", height = "300px")), 
-                        tabPanel("Average Winter Temperature", plotOutput(outputId = "awtEcoTypePlot", height = "300px"), plotOutput(outputId = "awtHerdPlot", height = "300px")
+                        tabPanel("Frost Free Days", plotOutput(outputId = "ffdPlot", height = "300px")), 
+                        tabPanel("Precipitation as Snow", plotOutput(outputId = "pasPlot", height = "300px")), 
+                        tabPanel("Average Winter Temperature", plotOutput(outputId = "awtPlot", height = "300px")
                   )))
         )
       )
@@ -130,25 +140,33 @@ server <- function(input, output) {
   valueModal<-reactiveValues(atTable=NULL)
   caribouHerd<-reactive({as.character(input$map_shape_click$group)})
   caribouEcoType<-reactive({ herd_bound@data[which(herd_bound@data$herd_name == input$map_shape_click$group), ][[12]]})
-
+  
+  climateData<-reactive({
+    if(!is.null(caribouEcoType()[1])){
+      eco<-dplyr::filter (clime, ecotype == caribouEcoType()[1])
+      eco$group<-caribouEcoType()[1]
+      herd<-dplyr::filter (clime, herdname == caribouHerd())
+      herd$group<-caribouHerd()
+      data<-rbind(eco, herd)
+      data
+    } else {
+      data<-data.frame(0,0,0,0)
+      colnames(data)<-c("ecotype","herdname", "year","pct")
+    }
+  })
+  
+  becData<-reactive({
+    data<-dplyr::filter (bec, herdname == caribouHerd() | herdname == caribouEcoType()[1])
+    data
+  })
+  
   herdSelect<-reactive({
     herd_bound[which(herd_bound@data$herd_name == input$map_shape_click$group), ]
   }) 
 
   dist_data <- reactive({
     req(input$map_shape_click)
-    conn<-dbConnect(dbDriver("PostgreSQL"),dbname=dbname, host=host ,port=port ,user=user ,password=password)
-    sql.str = paste0(
-      "SELECT SUM(t.areaha) AS sumarea, t.herd_name ,t.harvestyr
-    FROM (
-      SELECT b.areaha, b.harvestyr, y.herd_name
-      FROM public.cns_cut_bl_polygon b, (SELECT * FROM public.gcbp_carib_polygon WHERE herd_name = '",caribouHerd(), "') y
-      WHERE ST_INTERSECTS(b.wkb_geometry, y.geom))t 
-      GROUP BY harvestyr, herd_name
-      ORDER BY  herd_name, harvestyr")
-    cb_sum<-dbGetQuery(conn, sql.str)
-    dbDisconnect(conn)
-    cb_sum<-rbind(cb_sum,c(0,NA,1910)) #Add 40 years prior to the first cutblock date in cns_polys (~1950)
+    cb_sum<-rbind(cb_sumALL[which(cb_sumALL$herd_name ==caribouHerd()),],c(0,NA,1910)) #Add 40 years prior to the first cutblock date in cns_polys (~1950)
     if(!is.null(cb_sum$harvestyr)){
       cb2<-tidyr::complete(cb_sum, harvestyr = full_seq(harvestyr,1), fill = list(sumarea = 0))
       cb2$Dist40<-zoo::rollapplyr(cb2$sumarea, 40, FUN = sum, fill=0)
@@ -195,7 +213,7 @@ server <- function(input, output) {
       addLegend("bottomright", pal = pal, values = c("Red/Threatened","Blue/Special","Blue/Threatened"), title = "Risk Status", opacity = 1) %>%
       addDrawToolbar(
             editOptions = editToolbarOptions(),
-            targetGroup='Caribou Zone',
+            targetGroup='Drawn Zone',
             circleOptions = FALSE,
             circleMarkerOptions = FALSE,
             rectangleOptions = FALSE,
@@ -203,16 +221,12 @@ server <- function(input, output) {
             singleFeature = F,
             polygonOptions = drawPolygonOptions(showArea=TRUE, shapeOptions=drawShapeOptions(fillOpacity = 0
                                                                         ,color = 'red'
-                                                                       ,weight = 3, clickable = TRUE)))%>%
-    addLayersControl(baseGroups = c("OpenStreetMap","WorldImagery"), overlayGroups = c('UWR','WHA', 'Caribou Zone', 'Caribou Selection'), options = layersControlOptions(collapsed = TRUE)) %>%
-    hideGroup(c('UWR','WHA', 'Caribou Selection'))%>%
-      htmlwidgets::onRender(JS("
-                               function(el, x, data){
-                               var map = this;
-                               Shiny.onInputChange(map.id+'_layers_added',map.layerManager);
-                               }"))
+                                                                       ,weight = 3, clickable = TRUE))) %>%
+    addLayersControl(baseGroups = c("OpenStreetMap","WorldImagery"), overlayGroups = c('UWR','WHA', 'Drawn Zone', 'Caribou Selection'), options = layersControlOptions(collapsed = TRUE)) %>%
+    hideGroup(c('UWR','WHA', 'Caribou Selection')) 
+
   })
-  
+ 
 # Create a shapefile to download
   output$downloadData.zip <- downloadHandler(
     file = 'shpExport.',
@@ -221,13 +235,13 @@ server <- function(input, output) {
         file.remove(Sys.glob("shpExport.*"))
       }
       #get the lat long coordinates
-      req(valueModal)
-      if(!is.null(input$map_draw_all_features)){
+    req(valueModal)
+    if(!is.null(input$map_draw_all_features)){
         f<-input$map_draw_all_features
         coordz<-lapply(f$features, function(x){unlist(x$geometry$coordinates)})
         
         Longitudes<-lapply(coordz, function(coordz) {coordz[seq(1,length(coordz), 2)] })
-        Latitudes<-lapply(coordz, function(coordz) { coordz[seq(2,length(coordz), 2)] })
+        Latitudes<-lapply(coordz, function(coordz)  {coordz[seq(2,length(coordz), 2)] })
         
         polys<-list()
         for (i in 1:length(Longitudes)){polys[[i]]<- Polygons(
@@ -248,92 +262,49 @@ server <- function(input, output) {
         SPDF<-SpatialPolygonsDataFrame(spPolys, data=p.df)
         plot(SPDF)
         rgdal::writeOGR(SPDF, dsn="shpExport.shp", layer="shpExport", driver="ESRI Shapefile")
-      }
-      zip(zipfile='shpExport.zip', files=Sys.glob("shpExport.*"))
-      file.copy("shpExport.zip", file)
-    })
+    }
+    zip(zipfile='shpExport.zip', files=Sys.glob("shpExport.*"))
+    file.copy("shpExport.zip", file)
+  })
   
-  output$becEcoTypePlot <- renderPlot ({
-      ggplot (dplyr::filter (bec, ecotype == caribouEcoType()[1]), 
-              aes (x = year)) +  
-        geom_bar (aes (fill = bec), position = 'fill') +
-        xlab ("Year") +
-        ylab ("Proportion of range area") +
+  output$becPlot <- renderPlot ({
+      ggplot (becData(), aes (x = herdname, y=pct, fill = bec)) + 
+        facet_wrap(~year)+
+        geom_bar (stat = "identity", position = "fill") +
+        xlab ("Boundary") +
+        ylab ("Proportion of Area (%)") +
         scale_fill_discrete (name = "Bec Zone") +
-        theme (axis.text = element_text (size = 12),
+        theme (axis.text = element_text (size = 12), legend.title = element_blank(),
                axis.title =  element_text (size = 14, face = "bold")) 
   })  
 
-  output$ffdEcoTypePlot <- renderPlot ({
-    ggplot (dplyr::filter (ffd, ecotype == caribouEcoType()[1]), 
-            aes (year, spffd)) +  
+  output$ffdPlot <- renderPlot ({
+    ggplot (climateData(), aes (year, sffd, fill = group)) +  
       geom_boxplot () +
       xlab ("Year") +
-      ylab ("Number of Spring Frost Free Days") +
-      theme (axis.text = element_text (size = 12),
+      ylab ("Spring Frost Free Days (#)") +
+      theme (axis.text = element_text (size = 12), legend.title = element_blank(),
              axis.title =  element_text (size = 14, face = "bold")) 
   }) 
   
-  output$pasEcoTypePlot <- renderPlot ({
-    ggplot (dplyr::filter (wpas, ecotype == caribouEcoType()[1]), 
-            aes (x=year, y=pas)) +  
+  output$pasPlot <- renderPlot ({
+    ggplot (climateData(), aes (x=year, y=pas, fill = group)) +  
       geom_boxplot () +
       xlab ("Year") +
-      ylab ("Precipitation as Snow") +
-      theme (axis.text = element_text (size = 12),
+      ylab ("Precipitation as Snow (mm)") +
+      theme (axis.text = element_text (size = 12), legend.title = element_blank(),
              axis.title =  element_text (size = 14, face = "bold")) 
   }) 
   
-  output$awtEcoTypePlot <- renderPlot ({
-    ggplot (dplyr::filter (wtemp, ecotype == caribouEcoType()[1]), 
-            aes (x=year, y=awt)) +  
+  output$awtPlot <- renderPlot ({
+    ggplot (climateData(), 
+            aes (x=year, y=awt, fill = group)) +  
       geom_boxplot () +
       xlab ("Year") +
-      ylab ("Average Winter Temperature") +
-      theme (axis.text = element_text (size = 12),
+      ylab ("Average Winter Temperature (°C)") +
+      theme (axis.text = element_text (size = 12), legend.title = element_blank(),
              axis.title =  element_text (size = 14, face = "bold")) 
   }) 
-  
-  output$becHerdPlot <- renderPlot ({
-    ggplot (dplyr::filter (bec, herdname == caribouHerd()), 
-            aes (x = year)) +  
-      geom_bar (aes (fill = bec), position = 'fill') +
-      xlab ("Year") +
-      ylab ("Proportion of range area") +
-      scale_fill_discrete (name = "Bec Zone") +
-      theme (axis.text = element_text (size = 12),
-             axis.title =  element_text (size = 14, face = "bold")) 
-  })  
-  
-  output$ffdHerdPlot <- renderPlot ({
-    ggplot (dplyr::filter (ffd, herdname == caribouHerd()), 
-            aes (year, spffd)) +  
-      geom_boxplot () +
-      xlab ("Year") +
-      ylab ("Number of Spring Frost Free Days") +
-      theme (axis.text = element_text (size = 12),
-             axis.title =  element_text (size = 14, face = "bold")) 
-  }) 
-  
-  output$pasHerdPlot <- renderPlot ({
-    ggplot (dplyr::filter (wpas, herdname== caribouHerd()), 
-            aes (year, pas)) +  
-      geom_boxplot () +
-      xlab ("Year") +
-      ylab ("Precipitation as Snow") +
-      theme (axis.text = element_text (size = 12),
-             axis.title =  element_text (size = 14, face = "bold")) 
-  }) 
-  
-  output$awtHerdPlot <- renderPlot ({
-    ggplot (dplyr::filter (wtemp, herdname == caribouHerd()), 
-            aes (x=year, y=awt)) +  
-      geom_boxplot () +
-      xlab ("Year") +
-      ylab ("Average Winter Temperature") +
-      theme (axis.text = element_text (size = 12),
-             axis.title =  element_text (size = 14, face = "bold")) 
-  })  
   
   
 #-------
---
+++ SQL/cb_sumALL.sql
@@ -0,0 +1,8 @@
+﻿Create TAble cb_sum As
+SELECT SUM(t.areaha) AS sumarea, t.herd_name ,t.harvestyr
+    FROM (
+      SELECT b.areaha, b.harvestyr, y.herd_name
+      FROM public.cns_cut_bl_polygon b, (SELECT * FROM public.gcbp_carib_polygon) y
+      WHERE ST_INTERSECTS(b.wkb_geometry, y.geom))t 
+      GROUP BY harvestyr, herd_name
+      ORDER BY  herd_name, harvestyr;
