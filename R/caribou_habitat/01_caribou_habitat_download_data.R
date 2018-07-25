12b89038f983a9a0e9a4406006ef5e1ae35fd97e
Muhly <TMUHLY@BCGOV>
2018-07-25 15:46:01 -0700

    cleaned-up some code

--- R/caribou_habitat/01_caribou_habitat_download_data.R
+++ R/caribou_habitat/01_caribou_habitat_download_data.R
@@ -29,7 +29,7 @@
 #  Script Author: Tyler Muhly, Natural Resource Modeling Specialist, Forest Analysis and 
 #                 Inventory Branch, B.C. Ministry of Forests, Lands, and Natural Resource Operations.
 #                 Report is located here: 
-#  Script Date: 125 July 2018
+#  Script Date: 25 July 2018
 #  R Version: 3.4.3
 #  R Package Versions: 
 #  Data: 
@@ -37,138 +37,82 @@
 require (downloader)
 
 # data directory
-setwd ('C:\\Work\\caribou\\clus_data\\')
+setwd ('C:\\Work\\caribou\\clus_data\\caribou_habitat_model')
 
 #########################
 # MANUAL DATA DOWNLOADS #
 #########################
 
-# BEC zone climate projections; current, 2020s, 2050s and 2080s; downloaded 19 March 2018
-# <http://www.climatewna.com/ClimateBC_Map.aspx>
-# G:\!Workgrp\Analysts\tmuhly\Caribou\climate_analysis\data\bec\BEC_zone_2020s
-# G:\!Workgrp\Analysts\tmuhly\Caribou\climate_analysis\data\bec\BEC_zone_2050s
-# G:\!Workgrp\Analysts\tmuhly\Caribou\climate_analysis\data\bec\BEC_zone_2080s
+# copied from BCGW into geodatabase; for uploading to postgres
 
-# caribou range boudnaries; downloaded 19 March 2018
+# caribou range boundaries; downloaded 25 July 2018
 # <https://catalogue.data.gov.bc.ca/dataset/caribou-herd-locations-for-bc>
-# G:\!Workgrp\Analysts\tmuhly\Caribou\climate_analysis\data\caribou\caribou_herd
+# Name in GDB: boundary_caribou_pop_20180725
 
-# digital road atlas data; downloaded 29 March 2018
-# <https://catalogue.data.gov.bc.ca/dataset/digital-road-atlas-dra-demographic-partially-attributed-roads>
-# G:\!Workgrp\Analysts\tmuhly\Caribou\climate_analysis\data\roads\DRA_DGTL_ROAD_ATLAS_DPAR_SP
+# BEC Map; downloaded 25 July 2018
+# <https://catalogue.data.gov.bc.ca/dataset/biogeoclimatic-ecosystem-classification-bec-map>
+# Name in GDB: bec_poly_20180725
 
-# Oil and gas well/facility data; downloaded 29 March 2018
-# <https://catalogue.data.gov.bc.ca/dataset/oil-and-gas-commission-well-facility-area-permits>
-# G:\!Workgrp\Analysts\tmuhly\Caribou\climate_analysis\data\wells\OG_WELL_FACILITY_PERMIT_SP
-
-# Cutblock data; downloaded 29 March 2018
+# Cutblock data; downloaded 25 July 2018
 # <https://catalogue.data.gov.bc.ca/dataset/harvested-areas-of-bc-consolidated-cutblocks->
-# G:\!Workgrp\Analysts\tmuhly\Caribou\climate_analysis\data\cutblocks
+# Name in GDB: cutblocks_20180725
+
+# VRI; downloaded 25 July 2018
+# <https://catalogue.data.gov.bc.ca/dataset/vri-forest-vegetation-composite-polygons-and-rank-1-layer>
+# Name in GDB: VRI_20180725
+
+# Fire; downloaded 25 July 2018
+# <https://catalogue.data.gov.bc.ca/dataset/fire-perimeters-historical>
+# Name in GDB: fire_historic_20180725
+
+# Oil and gas well/facility; downloaded 25 July 2018
+# After 2016: <https://catalogue.data.gov.bc.ca/dataset/oil-and-gas-commission-facility-location-permits>
+# Name in GDB: oil_gas_facility_post2016_20180725
+# Before 2016: <https://catalogue.data.gov.bc.ca/dataset/oil-and-gas-commission-pre-2016-facility-locations>
+# Name in GDB: oil_gas_facility_pre2016_20180725
+
+# Pipelines; downloaded 25 July 2018
+# After 2006: <https://catalogue.data.gov.bc.ca/dataset/oil-and-gas-commission-pipeline-right-of-way-permits>
+# Name in GDB: pipeline_post2006_20180725
+# After 2016: <https://catalogue.data.gov.bc.ca/dataset/oil-and-gas-commission-pipeline-segment-permits>
+# Name in GDB: pipeline_post2016_20180725
+
+# Trasnmission Lines; downloaded 25 July 2018
+# <https://catalogue.data.gov.bc.ca/dataset/bc-transmission-lines>
+# Name in GDB: transmission_line_20180725
+
+# Railways; downloaded 25 July 2018
+# <https://catalogue.data.gov.bc.ca/dataset/railway-track-line>
+# Name in GDB: railway_20180725
+
+# Major Projects, for wind and mines; downloaded 25 July 2018
+# <https://catalogue.data.gov.bc.ca/dataset/natural-resource-sector-major-projects-points>
+# Name in GDB: major_projects_20180725
 
 # Integrated roads data from cumulative effects; copied June 18, 2018
 # \\spatialfiles.bcgov\\work\\srm\\bcce\\shared\\data_library\roads\2017\BC_CE_IntegratedRoads_2017_v1_20170214.gdb
-# layer = "integrated_roads"
-
+# Get from Kyle
 
 ###################################
 # Data downloadable from websites #
 ###################################
-# bc boundary; downloaded 19 March 2018
+# bc boundary; downloaded 25 July 2018
 # available from federal government via https://open.canada.ca/data/dataset/bab06e04-e6d0-41f1-a595-6cff4d71bedf
 download ("http://www12.statcan.gc.ca/census-recensement/2011/geo/bound-limit/files-fichiers/gpr_000b11a_e.zip", 
           dest = "province\\border.zip", 
           mode = "wb")
 unzip ("province\\border.zip", 
-       exdir = "C:\\Work\\caribou\\climate_analysis\\data\\province")
+       exdir = "C:\\Work\\caribou\\clus_data\\caribou_habitat_model\\province")
 file.remove ("province\\border.zip")
 
-# VRI
-download ("https://pub.data.gov.bc.ca/datasets/2ebb35d8-c82f-4a17-9c96-612ac3532d55/VEG_COMP_LYR_R1_POLY.gdb.zip",
-          dest = "vri\\vri.zip",
-          mode = "wb")
-unzip ("vri\\vri.zip",
-        exdir = "C:\\Work\\caribou\\climate_analysis\\data\\vri")
-file.remove ("vri\\vri.zip")
-
-
-# current, past and future climate measure projections; downloaded 28 March 2018
+# current climate measures; downloaded 25 July 2018
 # Reference: <http://climatebcdata.climatewna.com/#3._reference> # Wang, T., Hamann, A., Spittlehouse, D.L., Murdock, T., 2012. ClimateWNA - High-Resolution Spatial Climate Data for Western North America. Journal of Applied Meteorology and Climatology, 51: 16-29.
-  download ("http://climatebcdata.climatewna.com/download/Normal_1961_1990MSY/Normal_1961_1990_seasonal.zip", 
-            dest = "climate\\Normal_1961_1990_seasonal.zip", 
-            mode = "wb")
-unzip ("climate\\Normal_1961_1990_seasonal.zip", 
-       exdir = "G:\\!Workgrp\\Analysts\\tmuhly\\Caribou\\climate_analysis\\data\\climate")
-file.remove ("climate\\Normal_1961_1990_seasonal.zip")
-
 download ("http://climatebcdata.climatewna.com/download/Normal_1981_2010MSY/Normal_1981_2010_seasonal.zip", 
           dest = "climate\\Normal_1981_2010_seasonal.zip", 
           mode = "wb")
 unzip ("climate\\Normal_1981_2010_seasonal.zip", 
-       exdir = "G:\\!Workgrp\\Analysts\\tmuhly\\Caribou\\climate_analysis\\data\\climate")
+       exdir = "C:\\Work\\caribou\\clus_data\\caribou_habitat_model\\climate")
 file.remove ("climate\\Normal_1981_2010_seasonal.zip")
-
-download ("http://climatebcdata.climatewna.com/download/HadGEM2-ES_RCP45_2085MSY/HadGEM2-ES_RCP45_2085_seasonal.zip", 
-          dest = "climate\\HadGEM2-ES_RCP45_2085_seasonal.zip", 
-          mode = "wb")
-unzip ("climate\\HadGEM2-ES_RCP45_2085_seasonal.zip", 
-       exdir = "G:\\!Workgrp\\Analysts\\tmuhly\\Caribou\\climate_analysis\\data\\climate")
-file.remove ("climate\\HadGEM2-ES_RCP45_2085_seasonal.zip")  
-
-download ("http://climatebcdata.climatewna.com/download/HadGEM2-ES_RCP45_2055MSY/HadGEM2-ES_RCP45_2055_seasonal.zip", 
-          dest = "climate\\HadGEM2-ES_RCP45_2055_seasonal.zip", 
-          mode = "wb")
-unzip ("climate\\HadGEM2-ES_RCP45_2055_seasonal.zip", 
-       exdir = "G:\\!Workgrp\\Analysts\\tmuhly\\Caribou\\climate_analysis\\data\\climate")
-file.remove ("climate\\HadGEM2-ES_RCP45_2055_seasonal.zip") 
-
-download ("http://climatebcdata.climatewna.com/download/HadGEM2-ES_RCP45_2025MSY/HadGEM2-ES_RCP45_2025_seasonal.zip", 
-          dest = "climate\HadGEM2-ES_RCP45_2025_seasonal.zip", 
-          mode = "wb")
-unzip ("climate\\HadGEM2-ES_RCP45_2025_seasonal.zip", 
-       exdir = "G:\\!Workgrp\\Analysts\\tmuhly\\Caribou\\climate_analysis\\data\\climate")
-file.remove ("climate\\HadGEM2-ES_RCP45_2025_seasonal.zip")
-
-download ("http://climatebcdata.climatewna.com/download/CCSM4_RCP45_2085MSY/CCSM4_RCP45_2085_seasonal.zip", 
-          dest = "climate\CCSM4_RCP45_2085_seasonal.zip", 
-          mode = "wb")
-unzip ("climate\\CCSM4_RCP45_2085_seasonal.zip", 
-       exdir = "G:\\!Workgrp\\Analysts\\tmuhly\\Caribou\\climate_analysis\\data\\climate")
-file.remove ("climate\\CCSM4_RCP45_2085_seasonal.zip")
-
-download ("http://climatebcdata.climatewna.com/download/CCSM4_RCP45_2055MSY/CCSM4_RCP45_2055_seasonal.zip", 
-          dest = "climate\CCSM4_RCP45_2055_seasonal.zip", 
-          mode = "wb")
-unzip ("climate\\CCSM4_RCP45_2055_seasonal.zip", 
-       exdir = "G:\\!Workgrp\\Analysts\\tmuhly\\Caribou\\climate_analysis\\data\\climate")
-file.remove ("climate\\CCSM4_RCP45_2055_seasonal.zip")
-
-download ("http://climatebcdata.climatewna.com/download/CCSM4_RCP45_2025MSY/CCSM4_RCP45_2025_seasonal.zip", 
-          dest = "climate\CCSM4_RCP45_2025_seasonal.zip", 
-          mode = "wb")
-unzip ("climate\\CCSM4_RCP45_2025_seasonal.zip", 
-       exdir = "G:\\!Workgrp\\Analysts\\tmuhly\\Caribou\\climate_analysis\\data\\climate")
-file.remove ("climate\\CCSM4_RCP45_2025_seasonal.zip")
-
-download ("http://climatebcdata.climatewna.com/download/CanESM2_RCP45_2085MSY/CanESM2_RCP45_2085_seasonal.zip", 
-          dest = "climate\CanESM2_RCP45_2085_seasonal.zip", 
-          mode = "wb")
-unzip ("climate\CanESM2_RCP45_2085_seasonal.zip", 
-       exdir = "G:\\!Workgrp\\Analysts\\tmuhly\\Caribou\\climate_analysis\\data\\climate")
-file.remove ("climate\CanESM2_RCP45_2085_seasonal.zip")
-
-download ("http://climatebcdata.climatewna.com/download/CanESM2_RCP45_2055MSY/CanESM2_RCP45_2055_seasonal.zip", 
-          dest = "climate\CanESM2_RCP45_2055_seasonal.zip", 
-          mode = "wb")
-unzip ("climate\CanESM2_RCP45_2055_seasonal.zip", 
-       exdir = "G:\\!Workgrp\\Analysts\\tmuhly\\Caribou\\climate_analysis\\data\\climate")
-file.remove ("climate\CanESM2_RCP45_2055_seasonal.zip")
-
-download ("http://climatebcdata.climatewna.com/download/CanESM2_RCP45_2025MSY/CanESM2_RCP45_2025_seasonal.zip", 
-          dest = "climate\CanESM2_RCP45_2025_seasonal.zip", 
-          mode = "wb")
-unzip ("climate\CanESM2_RCP45_2025_seasonal.zip", 
-       exdir = "G:\\!Workgrp\\Analysts\\tmuhly\\Caribou\\climate_analysis\\data\\climate")
-file.remove ("climate\CanESM2_RCP45_2025_seasonal.zip")
 
 ###########################
 # Caribou telemetry data #
