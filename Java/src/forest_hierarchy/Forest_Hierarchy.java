8ea26812857df31fa94aeda1837c9cee2be89477
Lochhead <KLOCHHEA@BCGOV>
2018-11-06 11:16:54 -0800

    fixed - duplicate pixel in blockList from seedNew = 0. Set seedNew = -1

--- Java/src/forest_hierarchy/Forest_Hierarchy.java
+++ Java/src/forest_hierarchy/Forest_Hierarchy.java
@@ -37,11 +37,13 @@ public class Forest_Hierarchy {
 		int[] pixelBlock = new int[(int)(edgeList.size() + 1)];
 		Arrays.fill(pixelBlock, EMPTY);
 		
-		int blockID = 0, blockSize = 0, seed = 0, seedNew =0, d = 1;
+		int blockID = 0, blockSize = 0, seed = 0, seedNew = -1, d = 1;
 		int nTarget =  hist.bins.get(hist.getLastBin()-1).n;
 		double maxTargetSize = hist.bins.get(hist.getLastBin()-1).max_block_size;
 		boolean findBlocks=	!hist.bins.isEmpty(); //if there is a histogram with bins then findBlocks
 		degreeList = Arrays.asList(degree);
+		
+		edgeList.sort((o1, o2) -> Double.compare(o1.getWeight(), o2.getWeight()));
 		
 		//as long as the distribution of block sizes has not been met or there are edges to include, cluster pixels into blocks
 		while(findBlocks){
@@ -71,19 +73,19 @@ public class Forest_Hierarchy {
 
 			if(blockSize < maxTargetSize && nTarget > 0){
 				System.out.println("degree:" + degreeList.get(seed).intValue() );
-				if(seedNew >= 0){
-					if(seedNew == 0) {
+				if(seedNew >= -1){
+					if(seedNew == -1) {
 						blockList.add(seed + 1);
 						System.out.println("block list: " + (seed + 1));
 					}else {
 						blockList.add(seedNew + 1);
 						System.out.println("block list: " + (seedNew + 1));
-						seedNew =0;
+						seedNew = -1;
 					}
 					blockSize ++;
 				}else{
 					blockSize = (int) maxTargetSize;
-					seedNew = 0;
+					seedNew = -1;
 				}
 			}
 			
@@ -126,6 +128,8 @@ public class Forest_Hierarchy {
 		}
 		
 		setBlockPixels(pixelBlock);
+		
+		//little garbage collection?
 		pixelBlock = null;
 		blockList.clear();
 		edgeList.clear();
@@ -137,7 +141,7 @@ public class Forest_Hierarchy {
 	private static int findPixelToAdd(int seed) {
 	int nextPixel = -1;
 		for(Edges edge : edgeList){ //find the next pixel from a seed
-			if (edge.to == (seed +1)|| edge.from == (seed+1)) {
+			if (edge.to == (seed +1) || edge.from == (seed+1)) {
 				if(edge.to == (seed+1)) nextPixel = edge.from; //get the 'from' pixel because the seed is the 'to'
 				if(edge.from == (seed+1)) nextPixel = edge.to; //get the 'to' pixel because the seed is the 'from'
 				edgeList.remove(edge);
@@ -177,7 +181,7 @@ public class Forest_Hierarchy {
 			 edgeList.add( new Edges((int)row[0], (int)row[1], (double)row[2]));
 		}
 		System.out.println(dfList.size() + " edges have been added");
-		edgeList.sort((o1, o2) -> Double.compare(o1.getWeight(), o2.getWeight()));
+		
 
 		degree=Arrays.stream(dg).boxed().toArray( Integer[]::new );
 		System.out.println(degree.length + " pixel degrees have been added");
@@ -197,31 +201,31 @@ public class Forest_Hierarchy {
      */
 	public void createData() {
 			System.out.println("making up the data");
-			edgeList.add(new Edges(14,19,0.004068539));
-			edgeList.add(new Edges(15,20,0.007603801));
-			edgeList.add(new Edges(8,13,0.026225054));
-			edgeList.add(new Edges(11,12,0.029417822));
-			edgeList.add(new Edges(13,17,0.030595662));
-			edgeList.add(new Edges(8,9,0.031683749));
-			edgeList.add(new Edges(6,7,0.046285584));
-			edgeList.add(new Edges(3,9,0.056260681));
-			edgeList.add(new Edges(17,23,0.065944742));
-			edgeList.add(new Edges(1,2,0.106615236));
-			edgeList.add(new Edges(19,25,0.112814511));
-			edgeList.add(new Edges(5,10,0.139895661));
-			edgeList.add(new Edges(9,15,0.140727376));
-			edgeList.add(new Edges(24,25,0.141665573));
-			edgeList.add(new Edges(2,3,0.200729464));
-			edgeList.add(new Edges(17,21,0.217086723));
-			edgeList.add(new Edges(16,17,0.219919266));
-			edgeList.add(new Edges(9,14,0.245010326));
-			edgeList.add(new Edges(4,8,0.247409998));
-			edgeList.add(new Edges(7,13,0.257652422));
-			edgeList.add(new Edges(17,18,0.274287587));
-			edgeList.add(new Edges(16,22,0.285556721));
-			edgeList.add(new Edges(11,16,0.291724667));
-			edgeList.add(new Edges(10,14,0.322317448));
-		
+			edgeList.add(new Edges(1,7,0.086715709));
+			edgeList.add(new Edges(2,8,0.180796037));
+			edgeList.add(new Edges(3,4,0.308244033));
+			edgeList.add(new Edges(3,7,0.012837183));
+			edgeList.add(new Edges(5,9,0.1766306));
+			edgeList.add(new Edges(6,7,0.295186504));
+			edgeList.add(new Edges(7,11,0.122610174));
+			edgeList.add(new Edges(8,9,0.063785272));
+			edgeList.add(new Edges(8,13,0.10074124));
+			edgeList.add(new Edges(9,10,0.183048232));
+			edgeList.add(new Edges(9,14,0.278541127));
+			edgeList.add(new Edges(11,12,0.043365683));
+			edgeList.add(new Edges(11,16,0.091373383));
+			edgeList.add(new Edges(12,17,0.147566343));
+			edgeList.add(new Edges(13,18,0.091251474));
+			edgeList.add(new Edges(14,15,0.050895983));
+			edgeList.add(new Edges(17,21,0.129981886));
+			edgeList.add(new Edges(18,19,0.004794077));
+			edgeList.add(new Edges(18,22,0.022060849));
+			edgeList.add(new Edges(19,20,0.085095334));
+			edgeList.add(new Edges(20,24,0.408227721));
+			edgeList.add(new Edges(21,22,0.147379324));
+			edgeList.add(new Edges(23,24,0.230504445));
+			edgeList.add(new Edges(24,25,0.182275042));
+
 			degree = create_degree();
 			hist = new histogram();
 			blockEdges();
@@ -234,11 +238,11 @@ public class Forest_Hierarchy {
 	public static  Integer[]  create_degree() {
 		//System.out.println("adding degree array");
 		Integer[] degree = new Integer[25];
-		degree[0] = 1;degree[1] = 2;degree[2] = 2;degree[3] = 1;degree[4] = 1;
-		degree[5] = 1;degree[6] = 2;degree[7] = 3;degree[8] = 4;degree[9] = 2;
-		degree[10] = 2;degree[11] = 1;degree[12] = 3;degree[13] = 3;degree[14] = 2;
-		degree[15] = 3;degree[16] = 5;degree[17] = 1;degree[18] = 2;degree[19] = 1;
-		degree[20] = 1;degree[21] = 1;degree[22] = 1;degree[23] = 1;degree[24] = 2;
+		degree[0] = 1;degree[1] = 1;degree[2] = 2;degree[3] = 1;degree[4] = 1;
+		degree[5] = 1;degree[6] = 4;degree[7] = 3;degree[8] = 4;degree[9] = 1;
+		degree[10] = 3;degree[11] = 2;degree[12] = 2;degree[13] = 2;degree[14] = 1;
+		degree[15] = 1;degree[16] = 2;degree[17] = 3;degree[18] = 2;degree[19] = 2;
+		degree[20] = 2;degree[21] = 2;degree[22] = 1;degree[23] = 3;degree[24] = 1;
 		return degree;
 	}
 	
@@ -279,7 +283,7 @@ public class Forest_Hierarchy {
         
         public histogram() {
         	// TODO  need a way to make this discrete --issues with rounding up and down?
-/*        	areaBin ab  = new areaBin();
+/**/        areaBin ab  = new areaBin();
             ab.max_block_size = 2;
             ab.freq = 0.4;
             ab.n = 3;
@@ -289,13 +293,13 @@ public class Forest_Hierarchy {
             ab1.max_block_size = 3;
             ab1.freq = 0.28;
             ab1.n = 2;
-            bins.add(ab1);
+            bins.add(ab1); 
             
             areaBin ab11  = new areaBin();
             ab11.max_block_size = 4;
             ab11.freq = 0.2;
             ab11.n = 2;
-            bins.add(ab11); */
+            bins.add(ab11); 
             
             areaBin ab111  = new areaBin();
             ab111.max_block_size = 5; //changed this from 5, needed a large block that is more than the degrees
