bf736effbebacfb36fe62d1f0f033e75d999eb1a
Lochhead <KLOCHHEA@BCGOV>
2019-03-13 16:23:25 -0700

    fixed bug of NULL paths

--- R/SpaDES-modules/blockingCLUS/blockingCLUS.R
+++ R/SpaDES-modules/blockingCLUS/blockingCLUS.R
@@ -159,15 +159,13 @@ blockingCLUS.preBlock <- function(sim) {
   E(g)$weight<-edges.weight[,3]#assign weights to the graph. Requires a matrix input
   V(g)$name<-V(g) #assigns the name of the vertex - useful for maintaining link with raster
   
-  zones<-unname(unlist(dbGetQuery(sim$clusdb, 'Select distinct (zoneid) from pixels where thlb > 0  AND similar > 0 AND zoneid > 0 order by zoneid')))#get the zone names - strict use of integers for these
-  print(zones)
-  #patchSize<-unname(unlist(dbGetQuery(sim$clusdb, paste0("Select * FROM constraints where zoneid in (", zones, ")"))))
-  
-  #get the inputs for the forest_hierarchy java object as a list  
+  zones<-unname(unlist(dbGetQuery(sim$clusdb, 
+          'SELECT zoneid FROM pixels where thlb > 0 and similar > 0 group by zoneid Having count(pixelid) > 1')))#get the zone names - strict use of integers for these
+
+    #get the inputs for the forest_hierarchy java object as a list  
   resultset<-lapply(zones, function (x){ 
     vertices<-as.matrix(dbGetQuery(sim$clusdb, paste0('SELECT pixelid FROM pixels where zoneid = ', as.integer(x), ' AND thlb > 0 and similar > 0')))
     g.mst_sub<-mst(induced_subgraph(g, v = vertices), weighted=TRUE)
-    #print(E(g.mst_sub))
     paths.matrix<-data.table(cbind(noquote(get.edgelist(g.mst_sub)), E(g.mst_sub)$weight))
     paths.matrix[, V1 := as.integer(V1)]
     paths.matrix[, V2 := as.integer(V2)]
@@ -176,7 +174,7 @@ blockingCLUS.preBlock <- function(sim) {
     })
   
   #Run the forest_hierarchy java object in parallel. One for each 'zone'. This will maintain zone boundaries as block boundaries
-  if(length(zones) > 1 && object.size(g) > 10000000000){ #0.1 GB
+  if(length(zones) > 1 && object.size(g) > 10000000){ #0.1 GB
     noCores<-min(parallel::detectCores()-1, length(zones))
     print(paste0("make cluster on:", noCores, " cores"))
     cl<-makeCluster(noCores, type = "SOCK")
@@ -193,16 +191,17 @@ blockingCLUS.preBlock <- function(sim) {
   #Need to combine the results of blockids into clusdb. Update the pixels table and populate the blockids
   lastBlockID <<- 0
   result<-lapply(blockids, function(x){
-    test2<-x[][[1]][x[][[1]][,1]>-1,]
-    if(lastBlockID > 0){
-      test2[,1]<-test2[,1] + lastBlockID
-    } 
-    lastBlockID<<-max(test2[,1])
-    test2
+      test2<-x[][[1]][x[][[1]][,1]>-1,]
+      if(lastBlockID > 0){
+        test2[,1]<-test2[,1] + lastBlockID
+      } 
+      lastBlockID<<-max(test2[,1])
+      test2
   })
   
   blockids <- Reduce(function(...) merge(..., all=T), result)#join the list components
   blockids<-data.table(blockids)
+
   blockids[with(blockids, order(V2)), ] #sort the table
   blockids<-data.table(tidyr::complete(blockids, V2= seq(1:as.integer(length(sim$ras))),fill = list(V1 = as.integer(-1))))
   
@@ -288,7 +287,6 @@ blockingCLUS.spreadBlock<- function(sim) {
 ### additional functions
 getBlocksIDs<- function(x){
   print(paste0("getBlocksID for zone: ", x[][[3]]))
-  if(nrow(as.matrix(x[][[2]][,1])) > 0){
   .jinit(classpath= paste0(here::here(),"/Java/bin"), parameters="-Xmx 5g", force.init = TRUE)
   fhClass<-.jnew("forest_hierarchy.Forest_Hierarchy") # creates a forest hierarchy object
   
@@ -308,10 +306,7 @@ getBlocksIDs<- function(x){
     fhClass$blockEdges() # creates the blocks
     blockids<-cbind(convertToR(fhClass$getBlocks()), as.integer(unlist(dg[,1]))) #creates a link between pixelid and blockid
     fhClass$clearInfo()
-    } else{
-    blockids = NULL
-    print("zero paths matrix")
-  }
+
   list(blockids)
 }
 
