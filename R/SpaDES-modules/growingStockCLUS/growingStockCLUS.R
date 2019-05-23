5a6e6fbd272f79b45e56fc834fa3a48d73b6d5b3
Lochhead <Kyle.Lochhead@gov.bc.ca>
2019-05-22 17:20:54 -0700

    tried update with sqlite -- query working

--- R/SpaDES-modules/growingStockCLUS/growingStockCLUS.R
+++ R/SpaDES-modules/growingStockCLUS/growingStockCLUS.R
@@ -64,13 +64,32 @@ doEvent.growingStockCLUS = function(sim, eventTime, eventType) {
 }
 
 Init <- function(sim) {
+  #To do a linear interpolation between yields in SQLite
+  #Rise vs run to calc the slope of the secant line between the rounded floor and ceiling values for age and yield i.e., (y1-y2)/(x1-x2)
+  #Then multiply by the x value ie., slope*(age - floor.age) + floor.yield
+  #This might be more efficient with a large number of yields to interpolate - its slighly slower 1 second
+  #Maybe very large AOI it will be more efficient???
+  
+  #dbGetQuery(clusdb, "SELECT t.pixelid,
+  #(((k.tvol - y.tvol*1.0)/10)*(t.age - CAST(t.age/10 AS INT)*10))+ y.tvol as itvol,
+  #(((k.height - y.height*1.0)/10)*(t.age - CAST(t.age/10 AS INT)*10))+ y.height as iht
+  #FROM pixels t
+  #LEFT JOIN yields y 
+  #ON t.yieldid = y.yieldid AND CAST(t.age/10 AS INT)*10 = y.age
+  #LEFT JOIN yields k 
+  #ON t.yieldid = k.yieldid AND round(t.age/10+0.5)*10 = k.age WHERE t.age > 0")
+
+  #Note with any linear interpolation there is a bias for higher yields at younger ages (before cMAI) and lower yields at older ages (past cMAI)
   dat<-data.table(dbGetQuery(sim$clusdb, "SELECT yieldid, age, tvol FROM yields"))
   tab1<-data.table(dbGetQuery(sim$clusdb, "SELECT pixelid, yieldid, age FROM pixels WHERE age >= 0"))
   tab1[, vol:= lapply(.SD, function(x) {approx(dat[yieldid == .BY]$age, 
                                                            dat[yieldid == .BY]$tvol, 
                                                            xout=x, rule = 2)$y}), .SD = "age" , by=yieldid]
+  tab1[, ht:= lapply(.SD, function(x) {approx(dat[yieldid == .BY]$age, 
+                                              dat[yieldid == .BY]$height, 
+                                              xout=x, rule = 2)$y}), .SD = "age" , by=yieldid]
   dbBegin(sim$clusdb)
-    rs<-dbSendQuery(sim$clusdb, "UPDATE pixels SET vol = :vol where pixelid = :pixelid", tab1[,c("vol", "pixelid")])
+    rs<-dbSendQuery(sim$clusdb, "UPDATE pixels SET vol = :vol, height = :ht where pixelid = :pixelid", tab1[,c("vol", "ht", "pixelid")])
   dbClearResult(rs)
   dbCommit(sim$clusdb)
   
@@ -81,6 +100,7 @@ Init <- function(sim) {
   return(invisible(sim))
 }
 growingStockCLUS.Update<- function(sim) {
+  #Note: See the SQLiteapproach to updating. The Update statement does not support JOIN
   #update the age first
   print(paste0("update at ", time(sim)))
   dbBegin(sim$clusdb)
@@ -94,10 +114,16 @@ growingStockCLUS.Update<- function(sim) {
   tab1[, vol:= lapply(.SD, function(x) {approx(dat[yieldid == .BY]$age, 
                                                dat[yieldid == .BY]$tvol, 
                                                xout=x, rule = 2)$y}), .SD = "age" , by=yieldid]
+  tab1[, ht:= lapply(.SD, function(x) {approx(dat[yieldid == .BY]$age, 
+                                              dat[yieldid == .BY]$height, 
+                                              xout=x, rule = 2)$y}), .SD = "age" , by=yieldid]
   dbBegin(sim$clusdb)
-  rs<-dbSendQuery(sim$clusdb, "UPDATE pixels SET vol = :vol where pixelid = :pixelid", tab1[,c("vol", "pixelid")])
+  rs<-dbSendQuery(sim$clusdb, "UPDATE pixels SET vol = :vol, height = :ht where pixelid = :pixelid", tab1[,c("vol", "ht", "pixelid")])
   dbClearResult(rs)
   dbCommit(sim$clusdb)
+  
+  #Vacuum the db
+  dbExecute(sim$clusdb, "VACUUM;")
   
   rm(tab1,dat)
   gc()
