d3000fabe2d8f3d2af0c9e5eaaaa2d32b56787cb
Lochhead <Kyle.Lochhead@gov.bc.ca>
2019-06-26 16:03:22 -0700

    checking every pixel is associated with a table constraint

---
+++ SQL/checkingUWR.sql
@@ -0,0 +1,11 @@
+﻿SELECT * FROM public.zone_uwr 
+FULL OUTER JOIN
+(SELECT (pvc).value as value, SUM((pvc).count) as count
+ FROM (SELECT ST_ValueCount(rast,1) AS pvc
+   FROM rast.zone_uwr) AS f
+ GROUP BY value ORDER BY value) as h
+ ON zoneid = value
+ ORDER BY zoneid, value;
+
+
+ SELECT * FROM public.zone_uwr WHERE label = 'u-3-003';
