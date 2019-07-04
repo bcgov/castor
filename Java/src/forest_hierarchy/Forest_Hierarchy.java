288597990b1a92c33fee8a2a5e7c2ee6273f5006
Lochhead <Kyle.Lochhead@gov.bc.ca>
2019-07-04 11:55:09 -0700

    removed variation allowed

--- Java/src/forest_hierarchy/Forest_Hierarchy.java
+++ Java/src/forest_hierarchy/Forest_Hierarchy.java
@@ -156,14 +156,15 @@ public class Forest_Hierarchy {
 				if(edge.to == (seed + 1)) nextPixel = edge.from; //get the 'from' pixel because the seed is the 'to'
 				if(edge.from == (seed + 1)) nextPixel = edge.to; //get the 'to' pixel because the seed is the 'from'
 				
-				if(blocksize > 1 && edge.weight < allowableDiff){
+				//if(blocksize > 1 && (edge.weight - cwt) < allowableDiff){
 					this.edgeList.remove(edge);
 					break; //a match ha
-				}
-				if(blocksize <= 1){
-					this.edgeList.remove(edge);
-					break; //a match has been found so break out of the loop of the edges
-				}
+				//}
+				//if(blocksize <= 1){
+				//	cwt = edge.weight;
+				//	this.edgeList.remove(edge);
+				//	break; //a match has been found so break out of the loop of the edges
+				//}
 				
 			}
 		}
