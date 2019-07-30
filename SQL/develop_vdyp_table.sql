ae2a1fbcf7ca635b352e2967c3c673f88197643c
Lochhead <Kyle.Lochhead@gov.bc.ca>
2019-07-30 13:25:11 -0700

    init

---
+++ SQL/develop_vdyp_table.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE public.vdyp_err
+(
+  error text
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.vdyp_err
+  OWNER TO clus_project;
+/*Import the data*/
+Alter TAble vdyp_err Add Column feature_id integer;
+UPDATE vdyp_err SET feature_id = CAST(substring(error, position(': ' in error) + 2, position(' ) ' in error) - position(': ' in error)-2) AS integer );
+
+SELECT distinct (feature_id) from vdyp_err;
+
+/*DELETE FROM vdyp*/
+DELETE FROM vdyp WHERE feature_id IN
+(SELECT distinct (feature_id) from vdyp_err);
