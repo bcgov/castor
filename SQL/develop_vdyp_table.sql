ced12f12159b0d38d62131322b0970999b70c431
Lochhead <Kyle.Lochhead@gov.bc.ca>
2019-08-13 14:37:09 -0700

    merging inventory and projections

--- SQL/develop_vdyp_table.sql
+++ SQL/develop_vdyp_table.sql
@@ -222,7 +222,7 @@ ALTER TABLE vdyp_err Add Column dcode text;
 UPDATE vdyp_err SET code = substring(split_part(error, ' - ', 2),1,2), dcode = split_part(error, ' - ', 2) ;
 
 /*Remove only the errors*/
-DELETE FROM vdyp vdyp WHERE feature_id IN (SELECT feature_id FROM vdyp_err WHERE code = 'E '); 
+DELETE FROM vdyp WHERE feature_id IN (SELECT feature_id FROM vdyp_err WHERE code = 'E '); 
 
 /*--------------------------------------------------------------------------*/
 /*Step 5. Merge in VRI forest Attribute information for creating aggregated key. */
@@ -230,7 +230,10 @@ DELETE FROM vdyp vdyp WHERE feature_id IN (SELECT feature_id FROM vdyp_err WHERE
 CREATE TABLE vdyp_vri2018 AS SELECT feature_id, bec_zone_code, bclcs_level_3, basal_area, crown_closure, crown_closure_class_cd, proj_height_1, proj_height_class_cd_1, site_index, 
 line_3_tree_species, species_cd_1, species_pct_1, species_cd_2, species_pct_2, species_cd_3,species_pct_3, species_cd_4,species_pct_4, polygon_area, wkb_geometry  
 FROM public.veg_comp_lyr_r1_poly_2018
-WHERE bclcs_level_2 = 'T';
+WHERE bclcs_level_2 = 'T' AND species_pct_1 > 0;
+
+delete from vdyp_vri2018 where UPPER(species_cd_1) IN ('G','GP','GR','J','JD','JH','JR','K','KC','OA','OB','OC','OD','OE','OF','OG','OH','OI','Q','QE','QG','QW','R','RA','T','TW','U','UA','UP',
+'V','VB','VP','VS','VV','VW','W','WA','WB','WD','WP','WS','WT','X','XC','XH','YP','Z','ZC','ZH'); /*non commercial species*/ 
 
 CREATE INDEX vdyp_vri2018_feature_id_idx /*Create an index which will be used to link with the vdyp output*/
   ON public.vdyp_vri2018
@@ -241,75 +244,129 @@ CREATE INDEX vdyp_vri2018_feature_id_idx /*Create an index which will be used to
 The species symbols are F (Douglas fir), C (western red cedar), H (hemlock), B (balsam), S (spruce), Sb (black spruce), Yc (yellow cedar), P (pine), L (larch), 
 Ac (Populus), D (red alder), Mb (broadleaf maple), E (birch), O (non-commercial). 
 see https://www2.gov.bc.ca/assets/gov/farming-natural-resources-and-industry/forestry/stewardship/forest-analysis-inventory/data-management/standards/vegcomp_poly_rank1_data_dictionary_draft40.pdf*/ 
+
+/*We keep these codes and add in further decription as per VDYP species codes. Thus, Hw is western hemock versus mountain hemlock.*/
 Update vdyp_vri2018 set species_cd_1 =
 CASE 
-WHEN UPPER(species_cd_1) IN ('D','DG','DM','DR','E','EA','EB','EE','EP','ES','EW','EX','EXP','EXW') THEN 'D'
-WHEN UPPER(species_cd_1) IN ('E','EA','EB','EE','EP','ES','EW','EX','EXP','EXW') THEN 'E'
-WHEN UPPER(species_cd_1) IN ('A','AC','ACB','ACT','AD','AT','AX') THEN 'Ac'
-WHEN UPPER(species_cd_1) IN ('B','BA','BB','BC','BG','BL','BM','BP') THEN 'B'
-WHEN UPPER(species_cd_1) IN ('C','CW') THEN 'C'
-WHEN UPPER(species_cd_1) IN ('Y','YC') THEN 'Yc'
-WHEN UPPER(species_cd_1) IN ('F','FD','FDC','FDI') THEN 'F'
-WHEN UPPER(species_cd_1) IN ('H','HM','HW','HX','HXM') THEN 'H'
-WHEN UPPER(species_cd_1) IN ('L','LA','LD','LS','LT','LW') THEN 'L'
-WHEN UPPER(species_cd_1) IN ('M','MB','ME','MN','MR','MS','MV') THEN 'Mb'
-WHEN UPPER(species_cd_1) IN ('G','GP','GR','J','JD','JH','JR','K','KC','OA','OB','OC','OD','OE','OF','OG','OH','OI','Q','QE','QG','QW','R','RA','T','TW','U','UA','UP',
-'V','VB','VP','VS','VV','VW','W','WA','WB','WD','WP','WS','WT','X','XC','XH','YP','Z','ZC','ZH') THEN 'O'
-WHEN UPPER(species_cd_1) IN ('P','PA','PF','PJ','PL','PLC','PLI','PM','PR','PS','PW','PX','PXJ','PY') THEN 'P'
-WHEN UPPER(species_cd_1) IN ('SB') THEN 'Sb'
-WHEN UPPER(species_cd_1) IN ('S','SA','SE','SN','SS','SW','SX','SXB','SXE','SXL','SXS','SXW','SXX') THEN 'S'
+WHEN UPPER(species_cd_1) IN ('D','DG','DM','DR') THEN 'Dr' /*Red alder*/
+WHEN UPPER(species_cd_1) IN ('E','EB','EE','ES','EW','EX','EXP','EXW') THEN 'E' /*Birch*/
+WHEN UPPER(species_cd_1) IN ('EA') THEN 'Ea' /*Alaskan Birch*/
+WHEN UPPER(species_cd_1) IN ('EP') THEN 'Ep' /*Common paper Birch*/
+WHEN UPPER(species_cd_1) IN ('A','AC','ACB','ACT','AD','AX') THEN 'Ac' /*Poplar*/
+WHEN UPPER(species_cd_1) IN ('AT') THEN 'At' /*aspen*/
+WHEN UPPER(species_cd_1) IN ('B','BB','BC','BM','BP') THEN 'B' /*True Fir*/
+WHEN UPPER(species_cd_1) IN ('BL') THEN 'Bl' /*Apline fir*/
+WHEN UPPER(species_cd_1) IN ('BG') THEN 'Bg' /*grand fir*/
+WHEN UPPER(species_cd_1) IN ('BA') THEN 'Ba' /*amabillis fir*/
+WHEN UPPER(species_cd_1) IN ('C','CW') THEN 'Cw' /*Western Red Cedar*/
+WHEN UPPER(species_cd_1) IN ('Y','YC') THEN 'Yc' /*Yellow Cedar*/
+WHEN UPPER(species_cd_1) IN ('F','FD','FDC','FDI') THEN 'Fd' /*Douglas Fir*/
+WHEN UPPER(species_cd_1) IN ('H','HX','HXM') THEN 'H' /*Hemlock*/
+WHEN UPPER(species_cd_1) IN ('HM') THEN 'Hm' /*Mountain hemlock*/
+WHEN UPPER(species_cd_1) IN ('HW') THEN 'Hw' /*western hemlock*/
+WHEN UPPER(species_cd_1) IN ('L','LD','LS','LW') THEN 'L' /*larch*/
+WHEN UPPER(species_cd_1) IN ('LW') THEN 'Lw' /*western larch*/
+WHEN UPPER(species_cd_1) IN ('LA') THEN 'La' /*alpine larch*/
+WHEN UPPER(species_cd_1) IN ('LT') THEN 'Lt' /*tamarack*/
+WHEN UPPER(species_cd_1) IN ('M','MB','ME','MN','MR','MS','MV') THEN 'Mb' /*big leaf maple*/
+WHEN UPPER(species_cd_1) IN ('P','PM','PR','PS','PX') THEN 'P'
+WHEN UPPER(species_cd_1) IN ('PL','PLC','PLI') THEN 'Pl' /*Lodgepole pine*/
+WHEN UPPER(species_cd_1) IN ('PY') THEN 'Py' /*yellow (ponderosa) pine*/
+WHEN UPPER(species_cd_1) IN ('PW') THEN 'Pw' /*westrern white pine*/
+WHEN UPPER(species_cd_1) IN ('PJ','PXJ') THEN 'Pj' /*jack pine*/
+WHEN UPPER(species_cd_1) IN ('PF') THEN 'Pf' /*limber pine*/
+WHEN UPPER(species_cd_1) IN ('PA') THEN 'Pa' /*whitebark pine*/
+WHEN UPPER(species_cd_1) IN ('SB') THEN 'Sb' /*black spruce*/
+WHEN UPPER(species_cd_1) IN ('SS') THEN 'Ss' /*sitka spruce*/
+WHEN UPPER(species_cd_1) IN ('SE','SXE') THEN 'Se' /*engelmann spruce*/
+WHEN UPPER(species_cd_1) IN ('SW','SXW') THEN 'Sw' /*white spruce*/
+WHEN UPPER(species_cd_1) IN ('S','SA','SN','SX','SXB','SXL','SXS','SXX') THEN 'S' /*generic spruce?*/
+ELSE species_cd_1
 END  ;
 
 Update vdyp_vri2018 set species_cd_2 =
 CASE 
-WHEN UPPER(species_cd_2) IN ('D','DG','DM','DR','E','EA','EB','EE','EP','ES','EW','EX','EXP','EXW') THEN 'D'
-WHEN UPPER(species_cd_2) IN ('E','EA','EB','EE','EP','ES','EW','EX','EXP','EXW') THEN 'E'
-WHEN UPPER(species_cd_2) IN ('A','AC','ACB','ACT','AD','AT','AX') THEN 'Ac'
-WHEN UPPER(species_cd_2) IN ('B','BA','BB','BC','BG','BL','BM','BP') THEN 'B'
-WHEN UPPER(species_cd_2) IN ('C','CW') THEN 'C'
-WHEN UPPER(species_cd_2) IN ('Y','YC') THEN 'Yc'
-WHEN UPPER(species_cd_2) IN ('F','FD','FDC','FDI') THEN 'F'
-WHEN UPPER(species_cd_2) IN ('H','HM','HW','HX','HXM') THEN 'H'
-WHEN UPPER(species_cd_2) IN ('L','LA','LD','LS','LT','LW') THEN 'L'
-WHEN UPPER(species_cd_2) IN ('M','MB','ME','MN','MR','MS','MV') THEN 'Mb'
-WHEN UPPER(species_cd_2) IN ('G','GP','GR','J','JD','JH','JR','K','KC','OA','OB','OC','OD','OE','OF','OG','OH','OI','Q','QE','QG','QW','R','RA','T','TW','U','UA','UP',
-'V','VB','VP','VS','VV','VW','W','WA','WB','WD','WP','WS','WT','X','XC','XH','YP','Z','ZC','ZH') THEN 'O'
-WHEN UPPER(species_cd_2) IN ('P','PA','PF','PJ','PL','PLC','PLI','PM','PR','PS','PW','PX','PXJ','PY') THEN 'P'
-WHEN UPPER(species_cd_2) IN ('SB') THEN 'Sb'
-WHEN UPPER(species_cd_2) IN ('S','SA','SE','SN','SS','SW','SX','SXB','SXE','SXL','SXS','SXW','SXX') THEN 'S'
+WHEN UPPER(species_cd_2) IN ('D','DG','DM','DR') THEN 'Dr' /*Red alder*/
+WHEN UPPER(species_cd_2) IN ('E','EB','EE','ES','EW','EX','EXP','EXW') THEN 'E' /*Birch*/
+WHEN UPPER(species_cd_2) IN ('EA') THEN 'Ea' /*Alaskan Birch*/
+WHEN UPPER(species_cd_2) IN ('EP') THEN 'Ep' /*Common paper Birch*/
+WHEN UPPER(species_cd_2) IN ('A','AC','ACB','ACT','AD','AX') THEN 'Ac' /*Poplar*/
+WHEN UPPER(species_cd_2) IN ('AT') THEN 'At' /*aspen*/
+WHEN UPPER(species_cd_2) IN ('B','BB','BC','BM','BP') THEN 'B' /*True Fir*/
+WHEN UPPER(species_cd_2) IN ('BL') THEN 'Bl' /*Apline fir*/
+WHEN UPPER(species_cd_2) IN ('BG') THEN 'Bg' /*grand fir*/
+WHEN UPPER(species_cd_2) IN ('BA') THEN 'Ba' /*amabillis fir*/
+WHEN UPPER(species_cd_2) IN ('C','CW') THEN 'Cw' /*Western Red Cedar*/
+WHEN UPPER(species_cd_2) IN ('Y','YC') THEN 'Yc' /*Yellow Cedar*/
+WHEN UPPER(species_cd_2) IN ('F','FD','FDC','FDI') THEN 'Fd' /*Douglas Fir*/
+WHEN UPPER(species_cd_2) IN ('H','HX','HXM') THEN 'H' /*Hemlock*/
+WHEN UPPER(species_cd_2) IN ('HM') THEN 'Hm' /*Mountain hemlock*/
+WHEN UPPER(species_cd_2) IN ('HW') THEN 'Hw' /*western hemlock*/
+WHEN UPPER(species_cd_2) IN ('L','LD','LS','LW') THEN 'L' /*larch*/
+WHEN UPPER(species_cd_2) IN ('LW') THEN 'Lw' /*western larch*/
+WHEN UPPER(species_cd_2) IN ('LA') THEN 'La' /*alpine larch*/
+WHEN UPPER(species_cd_2) IN ('LT') THEN 'Lt' /*tamarack*/
+WHEN UPPER(species_cd_2) IN ('M','MB','ME','MN','MR','MS','MV') THEN 'Mb' /*big leaf maple*/
+WHEN UPPER(species_cd_2) IN ('P','PM','PR','PS','PX') THEN 'P'
+WHEN UPPER(species_cd_2) IN ('PL','PLC','PLI') THEN 'Pl' /*Lodgepole pine*/
+WHEN UPPER(species_cd_2) IN ('PY') THEN 'Py' /*yellow (ponderosa) pine*/
+WHEN UPPER(species_cd_2) IN ('PW') THEN 'Pw' /*westrern white pine*/
+WHEN UPPER(species_cd_2) IN ('PJ','PXJ') THEN 'Pj' /*jack pine*/
+WHEN UPPER(species_cd_2) IN ('PF') THEN 'Pf' /*limber pine*/
+WHEN UPPER(species_cd_2) IN ('PA') THEN 'Pa' /*whitebark pine*/
+WHEN UPPER(species_cd_2) IN ('SB') THEN 'Sb' /*black spruce*/
+WHEN UPPER(species_cd_2) IN ('SS') THEN 'Ss' /*sitka spruce*/
+WHEN UPPER(species_cd_2) IN ('SE','SXE') THEN 'Se' /*engelmann spruce*/
+WHEN UPPER(species_cd_2) IN ('SW','SXW') THEN 'Sw' /*white spruce*/
+WHEN UPPER(species_cd_2) IN ('S','SA','SN','SX','SXB','SXL','SXS','SXX') THEN 'S' /*generic spruce?*/
+ELSE species_cd_2
 END  ;
 
 Update vdyp_vri2018 set species_cd_3 =
 CASE 
-WHEN UPPER(species_cd_3) IN ('D','DG','DM','DR','E','EA','EB','EE','EP','ES','EW','EX','EXP','EXW') THEN 'D'
-WHEN UPPER(species_cd_3) IN ('E','EA','EB','EE','EP','ES','EW','EX','EXP','EXW') THEN 'E'
-WHEN UPPER(species_cd_3) IN ('A','AC','ACB','ACT','AD','AT','AX') THEN 'Ac'
-WHEN UPPER(species_cd_3) IN ('B','BA','BB','BC','BG','BL','BM','BP') THEN 'B'
-WHEN UPPER(species_cd_3) IN ('C','CW') THEN 'C'
-WHEN UPPER(species_cd_3) IN ('Y','YC') THEN 'Yc'
-WHEN UPPER(species_cd_3) IN ('F','FD','FDC','FDI') THEN 'F'
-WHEN UPPER(species_cd_3) IN ('H','HM','HW','HX','HXM') THEN 'H'
-WHEN UPPER(species_cd_3) IN ('L','LA','LD','LS','LT','LW') THEN 'L'
-WHEN UPPER(species_cd_3) IN ('M','MB','ME','MN','MR','MS','MV') THEN 'Mb'
-WHEN UPPER(species_cd_3) IN ('G','GP','GR','J','JD','JH','JR','K','KC','OA','OB','OC','OD','OE','OF','OG','OH','OI','Q','QE','QG','QW','R','RA','T','TW','U','UA','UP',
-'V','VB','VP','VS','VV','VW','W','WA','WB','WD','WP','WS','WT','X','XC','XH','YP','Z','ZC','ZH') THEN 'O'
-WHEN UPPER(species_cd_3) IN ('P','PA','PF','PJ','PL','PLC','PLI','PM','PR','PS','PW','PX','PXJ','PY') THEN 'P'
-WHEN UPPER(species_cd_3) IN ('SB') THEN 'Sb'
-WHEN UPPER(species_cd_3) IN ('S','SA','SE','SN','SS','SW','SX','SXB','SXE','SXL','SXS','SXW','SXX') THEN 'S'
+WHEN UPPER(species_cd_3) IN ('D','DG','DM','DR') THEN 'Dr' /*Red alder*/
+WHEN UPPER(species_cd_3) IN ('E','EB','EE','ES','EW','EX','EXP','EXW') THEN 'E' /*Birch*/
+WHEN UPPER(species_cd_3) IN ('EA') THEN 'Ea' /*Alaskan Birch*/
+WHEN UPPER(species_cd_3) IN ('EP') THEN 'Ep' /*Common paper Birch*/
+WHEN UPPER(species_cd_3) IN ('A','AC','ACB','ACT','AD','AX') THEN 'Ac' /*Poplar*/
+WHEN UPPER(species_cd_3) IN ('AT') THEN 'At' /*aspen*/
+WHEN UPPER(species_cd_3) IN ('B','BB','BC','BM','BP') THEN 'B' /*True Fir*/
+WHEN UPPER(species_cd_3) IN ('BL') THEN 'Bl' /*Apline fir*/
+WHEN UPPER(species_cd_3) IN ('BG') THEN 'Bg' /*grand fir*/
+WHEN UPPER(species_cd_3) IN ('BA') THEN 'Ba' /*amabillis fir*/
+WHEN UPPER(species_cd_3) IN ('C','CW') THEN 'Cw' /*Western Red Cedar*/
+WHEN UPPER(species_cd_3) IN ('Y','YC') THEN 'Yc' /*Yellow Cedar*/
+WHEN UPPER(species_cd_3) IN ('F','FD','FDC','FDI') THEN 'Fd' /*Douglas Fir*/
+WHEN UPPER(species_cd_3) IN ('H','HX','HXM') THEN 'H' /*Hemlock*/
+WHEN UPPER(species_cd_3) IN ('HM') THEN 'Hm' /*Mountain hemlock*/
+WHEN UPPER(species_cd_3) IN ('HW') THEN 'Hw' /*western hemlock*/
+WHEN UPPER(species_cd_3) IN ('L','LD','LS','LW') THEN 'L' /*larch*/
+WHEN UPPER(species_cd_3) IN ('LW') THEN 'Lw' /*western larch*/
+WHEN UPPER(species_cd_3) IN ('LA') THEN 'La' /*alpine larch*/
+WHEN UPPER(species_cd_3) IN ('LT') THEN 'Lt' /*tamarack*/
+WHEN UPPER(species_cd_3) IN ('M','MB','ME','MN','MR','MS','MV') THEN 'Mb' /*big leaf maple*/
+WHEN UPPER(species_cd_3) IN ('P','PM','PR','PS','PX') THEN 'P'
+WHEN UPPER(species_cd_3) IN ('PL','PLC','PLI') THEN 'Pl' /*Lodgepole pine*/
+WHEN UPPER(species_cd_3) IN ('PY') THEN 'Py' /*yellow (ponderosa) pine*/
+WHEN UPPER(species_cd_3) IN ('PW') THEN 'Pw' /*westrern white pine*/
+WHEN UPPER(species_cd_3) IN ('PJ','PXJ') THEN 'Pj' /*jack pine*/
+WHEN UPPER(species_cd_3) IN ('PF') THEN 'Pf' /*limber pine*/
+WHEN UPPER(species_cd_3) IN ('PA') THEN 'Pa' /*whitebark pine*/
+WHEN UPPER(species_cd_3) IN ('SB') THEN 'Sb' /*black spruce*/
+WHEN UPPER(species_cd_3) IN ('SS') THEN 'Ss' /*sitka spruce*/
+WHEN UPPER(species_cd_3) IN ('SE','SXE') THEN 'Se' /*engelmann spruce*/
+WHEN UPPER(species_cd_3) IN ('SW','SXW') THEN 'Sw' /*white spruce*/
+WHEN UPPER(species_cd_3) IN ('S','SA','SN','SX','SXB','SXL','SXS','SXX') THEN 'S' /*generic spruce?*/
+ELSE species_cd_3
 END  ;
 
+
 /* Group species that are the same*/
-select * from vdyp_vri2018 where species_cd_1 = species_cd_3  limit 100;
-Update vdyp_vri2018 set species_pct_1 = CASE
-WHEN species_cd_1 = species_cd_2 THEN (species_pct_1 + species_pct_2)
-WHEN species_cd_1 = species_cd_3 THEN (species_pct_1 + species_pct_3)
-END;
+Update vdyp_vri2018 set species_pct_1 = (species_pct_1 + species_pct_2) WHERE species_cd_1 = species_cd_2; 
+Update vdyp_vri2018 set species_pct_1 = (species_pct_1 + species_pct_3) WHERE species_cd_1 = species_cd_3;
 Update vdyp_vri2018 set species_cd_2 = NULL, species_pct_2 = NULL WHERE species_cd_1 = species_cd_2;
 Update vdyp_vri2018 set species_cd_3 = NULL, species_pct_3 = NULL WHERE species_cd_1 = species_cd_3;
-
-Update vdyp_vri2018 set species_pct_2 = CASE
-WHEN species_cd_2 = species_cd_3 THEN (species_pct_2 + species_pct_3)
-END;
+Update vdyp_vri2018 set species_pct_2 = (species_pct_2 + species_pct_3) WHERE species_cd_2 = species_cd_3 ;
 Update vdyp_vri2018 set species_cd_3 = NULL, species_pct_3 = NULL WHERE species_cd_2 = species_cd_3;
 
 /*Make the aggregated sepcies call*/
@@ -322,8 +379,14 @@ WHEN species_cd_3 IS NULL AND species_cd_2 IS NOT NULL AND species_pct_2 < 20 TH
 WHEN species_cd_3 IS NULL AND species_cd_2 IS NULL THEN species_cd_1
 END;
 
-SELECT * FROM vdyp_vri2018 LIMIT 2345;
+Update vdyp_vri2018 set site_index = round(site_index); /*round the site index to nearest meter*/
 
+Alter TAble vdyp_vri2018 Add Column yc_grp text ;
+Update vdyp_vri2018 set yc_grp = NULL;
+Update vdyp_vri2018 set yc_grp = CONCAT(bec_zone_code,'_',l3spp,'_', site_index,'_',crown_closure_class_cd,'_', proj_height_class_cd_1) WHERE bec_zone_code IS NOT NULL
+AND site_index > 0 and crown_closure_class_cd IS NOT NULL and proj_height_class_cd_1 IS NOT NULL;
+/*select * from vdyp_vri2018 where bec_zone_code is null limit 3;*/
+/*select yc_grp, sum(polygon_area) as area FROM vdyp_vri2018 group by yc_grp order by yc_grp;*/
 /* Height Classes
 1	0 – 10.4m
 2	10.5 – 19.4m
@@ -334,3 +397,9 @@ SELECT * FROM vdyp_vri2018 LIMIT 2345;
 7	55.5 – 64.4m
 8	> 65.5m*/
 
+create table vdyptest as (SELECT * from (select yc_grp, polygon_area, b.feature_id, prj_total_age, prj_site_index, prj_dom_ht, prj_lorey_ht, prj_diameter, prj_tph, prj_ba, prj_vol_ws, prj_vol_cu, prj_vol_d, prj_vol_dw, prj_vol_dwb
+FROM vdyp 
+FULL JOIN (SELECT feature_id, yc_grp, polygon_area FROM vdyp_vri2018) as b
+ON vdyp.feature_id = b.feature_id) as foo);
+
+select * from vdyptest limit 100;
