ef068461265f4b5aea03c7f05a1d3841253c0d85
Lochhead <Kyle.Lochhead@gov.bc.ca>
2019-08-20 09:40:47 -0700

    order the rs

--- SQL/checkRasters.sql
+++ SQL/checkRasters.sql
@@ -1,1 +1,1 @@
-﻿select r_table_name, srid, blocksize_x, blocksize_y, ST_AsText(ST_Envelope(extent)) from raster_columns;
+﻿select r_table_name, srid, blocksize_x, blocksize_y, ST_AsText(ST_Envelope(extent)) from raster_columns order by r_table_name;
