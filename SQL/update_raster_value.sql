f6b0fd3a539d69f74228c426510049db67c15a9a
Lochhead <Kyle.Lochhead@gov.bc.ca>
2019-08-26 16:32:00 -0700

    added alter table

--- SQL/update_raster_value.sql
+++ SQL/update_raster_value.sql
@@ -1,1 +1,2 @@
-﻿create table rast.tave_wt2 as (select ST_MapAlgebra(rast,1,'32BF', '[rast]/10') as rast from rast.tave_wt);
+﻿create table rast.tave_sm2 as (select ST_MapAlgebra(rast,1,'32BF', '[rast]/10') as rast from rast.tave_sm);
+alter table rast.tave_sm2  rename to tave_sm ;
