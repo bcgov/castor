2da5a55187899787764d15070278ca25837bcc02
Lochhead <Kyle.Lochhead@gov.bc.ca>
2019-09-03 12:21:10 -0700

    complete temperature updates x 10

--- SQL/update_raster_value.sql
+++ SQL/update_raster_value.sql
@@ -1,2 +1,2 @@
-﻿create table rast.tave_sm2 as (select ST_MapAlgebra(rast,1,'32BF', '[rast]/10') as rast from rast.tave_sm);
-alter table rast.tave_sm2  rename to tave_sm ;
+create table rast.mat2 as (select ST_MapAlgebra(rast,1,'32BF', '[rast]/10') as rast from rast.mat);
+alter table rast.mat2  rename to mat ;
