c7dc9837de47659f763a6529b23de3cf96e708e7
Lochhead <Kyle.Lochhead@gov.bc.ca>
2019-10-07 09:35:36 -0700

    rm deats

--- R/apps/clus_explorer/global.R
+++ R/apps/clus_explorer/global.R
@@ -1,35 +1,35 @@
 library(shiny)
 library(shinydashboard)
 library(shinydashboardPlus)
-library(shinyjs)
-library(shinyBS)
-library(shinyWidgets)
-library(shinyFiles)
-library(shinyauthr)
 library(plotly)
-library(RSQLite)
 library(leaflet)
+library(DBI)
 library(RPostgreSQL)
+library(rpostgis)
 library(data.table)
 library(bsplus)
-library(dplyr)
-library(glue)
+library(igraph)
+library(sf)
 
-user_base <- data.frame(
-  user = c("user1", "user2"),
-  password = c("pass1", "pass2"), 
-  permissions = c("admin", "standard"),
-  name = c("User One", "User Two"),
-  stringsAsFactors = FALSE,
-  row.names = NULL
-)
 
 getTableQuery<-function(sql){
-  conn<-DBI::dbConnect(dbDriver("PostgreSQL"), host='206.12.91.188', dbname = 'clus', port='5432', user='appuser', password='clus')
+  conn<-DBI::dbConnect(dbDriver("PostgreSQL"), host='206.12.91.188', dbname = 'clus', port='5432', user='appuser', password='sHcL5w9RTn8ZN3kc')
   on.exit(dbDisconnect(conn))
   dbGetQuery(conn, sql)
 }
 
+getSpatialQuery<-function(sql){
+  conn<-DBI::dbConnect(dbDriver("PostgreSQL"), host='206.12.91.188', dbname = 'clus', port='5432', user='appuser', password='sHcL5w9RTn8ZN3kc')
+  on.exit(dbDisconnect(conn))
+  st_read(conn, query = sql)
+}
 
-#map <- leaflet() %>% addTiles()
-availStudyAreas <- unlist(getTableQuery("Select nspname from pg_catalog.pg_namespace WHERE nspname NOT IN ('pg_toast', 'pg_temp_1', 'pg_toast_temp_1', 'pg_catalog','information_schema', 'topology', 'public')"), use.names = FALSE)
+
+getRasterQuery<-function(srcRaster){
+  conn<-DBI::dbConnect(dbDriver("PostgreSQL"), host='206.12.91.188', dbname = 'clus', port='5432', user='appuser', password='sHcL5w9RTn8ZN3kc')
+  on.exit(dbDisconnect(conn))
+  pgGetRast(conn, srcRaster)
+}
+
+
+availStudyAreas<-unlist(getTableQuery("Select nspname from pg_catalog.pg_namespace WHERE nspname NOT IN ('pg_toast', 'pg_temp_1', 'pg_toast_temp_1', 'pg_catalog','information_schema', 'topology', 'public')"), use.names = FALSE)
