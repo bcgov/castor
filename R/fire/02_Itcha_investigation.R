232899d7af023c1a9a8d03caa0e218527f6585ec
ElizabethKleynhans <Elizabeth.Kleynhans@gov.bc.ca>
2020-03-23 22:51:43 -0700

    Mostly automated code to estimate area burned per herd and habitat type

--- R/fire/02_Itcha_investigation.R
+++ R/fire/02_Itcha_investigation.R
@@ -8,55 +8,53 @@ require(rgeos)
 require(sp)
 options(scipen=999)
 
+#------------------------------------------------
 # read in fire data
+setwd("C:\\Work\\caribou\\clus_data\\Fire\\Results")
 fire.bound<-read.csv("C:\\Work\\caribou\\clus_data\\Fire\\fire_sum_crithab.csv",header=FALSE,col.names=c("area_m2","HERD_NAME","habitat","year"))
 head(fire.bound)
 
-Herd_name<-c("Central_Selkirks","Columbia_North","Groundhog", "Monashee", "Purcell_Central", "Purcell_South","South_Selkirks","Wells_Gray_South","Columbia_South","Hart_Ranges","North_Cariboo","Telkwa","Wells_Gray_North","Central_Rockies","Charlotte_Alplands","Itcha_Ilgachuz","Rainbows","Barkerville","Narrow_Lake","Frisby_Boulder","Redrock_Prairie_Creek")
+#Herd_name<-c("Central_Selkirks","Columbia_North","Groundhog", "Monashee", "Purcell_Central", "Purcell_South","South_Selkirks","Wells_Gray_South","Columbia_South","Hart_Ranges","North_Cariboo","Telkwa","Wells_Gray_North","Central_Rockies","Charlotte_Alplands","Itcha_Ilgachuz","Rainbows","Barkerville","Narrow_Lake","Frisby_Boulder","Redrock_Prairie_Creek")
+
+Herd_name<-c("Wells_Gray_North","Charlotte_Alplands","Itcha_Ilgachuz","Rainbows","Barkerville")
 Years<-1919:2018
-habitat_types<-c("HEWSR","Matrix","LEWR","LESR")
-window_size<-40
-
-Itcha_fire<-fire.bound %>%
-  filter(year>=1978, HERD_NAME=="Itcha_Ilgachuz")
-
-mu <- ddply(Itcha_fire, "habitat", summarise, grp.mean=mean(area_m2))
-# calculating 95% confidence interval . Used Whitlock and Schluter pg 309 (CI for mean of normal distribution. Probably not right so should consider this further.But for now...)
-habitat<-c("Matrix","HEWSR","LESR", "LEWR")
-grp.95.CI<-c(14076+2.04*(56011/sqrt(32)),2376+2.31*(3154/sqrt(9)), 5746+2.31*(12521/sqrt(9)),5136+2.05*(19078/sqrt(28)))
-grp.90.CI<-c(14076+1.7*(56011/sqrt(32)),2376+1.86*(3154/sqrt(9)), 5746+1.86*(12521/sqrt(9)),5136+1.7*(19078/sqrt(28)))
-m4<-as.data.frame(cbind(habitat,grp.95.CI,grp.90.CI))
-m4$grp.95.CI<-as.numeric(as.character(m4$grp.95.CI))
-m4$grp.90.CI<-as.numeric(as.character(m4$grp.90.CI))
-
-head(mu)
-
-ggplot(Itcha_fire, aes(x = area_m2/10000)) + 
-  theme_bw() +
-  theme(text = element_text(size=18))+
-  geom_histogram(bins=60) +
-    facet_grid(habitat~.) +
-  geom_vline(data=mu, aes(xintercept=grp.mean/10000 ,color="red"),
-             linetype="dashed")+
-  labs(y="Frequency", x="Area burned (ha^2)")+ 
-  geom_vline(data=mu4, aes(xintercept=grp.95.CI/10000))
-  
-
+habitat_types<-c("HEWSR","LESR","LEWR","Matrix")
 fire.bound$count<-1
-Itcha_summary<-fire.bound %>%
-  filter(year>=1978, HERD_NAME=="Itcha_Ilgachuz") %>%
+fire.bound$area_m2<-as.numeric(fire.bound$area_m2)
+
+for (i in 1:length(Herd_name)){
+
+Fire_area<-fire.bound %>%
+  filter(year>=1978, HERD_NAME==Herd_name[i]) 
+
+burn_per_area<- Fire_area %>%
   group_by(habitat) %>%
-  summarise(mean_ha2=mean(area_m2/10000),
-          median_ha2=median(area_m2/10000),
-          max_ha2=max(area_m2/10000),
-          min_ha2=min(area_m2/10000),
-          #sd_ha=sd(area_m2/10000),
-         # number=sum(count))
-          #CI_95_upper_ha2=mean(area_m2/10000)+(1.96*(sd(area_m2/10000)/sqrt(sum(count)))),
-          #CI_90_upper_ha2=mean(area_m2/10000)+(1.64*(sd(area_m2/10000)/sqrt(sum(count))))
-          )
+  summarise(mean_ha2=(mean(area_m2))/10000,
+            median_ha2=(median(area_m2)/10000),
+            max_ha2=(max(area_m2))/10000,
+            CI_95_upper_ha2=(mean(area_m2)+(1.96*(sd(area_m2)/sqrt(sum(count)))))/10000,
+            CI_90_upper_ha2=(mean(area_m2)+(1.64*(sd(area_m2)/sqrt(sum(count)))))/10000
+  )
 
+plot<-ggplot(Fire_area, aes(x = area_m2/10000)) + 
+  theme_bw() +
+  theme(text = element_text(size=16))+
+  geom_histogram(bins=50) +
+  facet_grid(habitat~.) +
+  labs(y="Frequency", x="Area burned (ha^2)")+ 
+  geom_vline(data=burn_per_area, aes(xintercept=mean_ha2))+
+  geom_vline(data=burn_per_area, aes(xintercept=CI_95_upper_ha2),
+             linetype="dashed")
 
+nam<-paste("Plot",Herd_name[i],"pdf",sep=".")
+assign(nam,plot)
+
+ggsave(nam)
+
+}
+
+#####
+# Calculating the total area of each habitat type
 ## BC herd areas
 
 BC_caribou_habitat<-st_read(dsn="T:\\FOR\\VIC\\HTS\\ANA\\PROJECTS\\CLUS\\Data\\caribou\\bc_critical_habitat\\BC_caribou_core_matrix_habitat_v20190904_1_shp\\BC_caribou_core_matrix_habitat_v20190904_1\\BC_caribou_core_matrix_habitat_v20190904_1.shp", stringsAsFactors = T,quiet=TRUE)
@@ -66,29 +64,24 @@ plot(BC_caribou_habitat,max.plot=1)
 Itcha<-BC_caribou_habitat %>% 
   filter(Herd_Name=="Itcha-Ilgachuz") %>%
   group_by(BCHab_code) %>%
-  summarise(total.area<-sum(Area_Ha)) %>%
+  summarise(total.area=sum(Area_Ha)) %>%
   st_set_geometry(NULL)
 
-Areas<-c(143883,874604,746573,210420)
 
 for (i in 1:length(habitat_types)){
   
 tab<-as.data.frame(Itcha_summary %>% filter(habitat==habitat_types[i]) %>%
-  mutate(mean_area_percent=mean_ha2/Areas[i]*100,
-         median_area_percent = median_ha2/Areas[i] *100,
-         max_area_percent=max_ha2/Areas[i]*100,
-         min_area_percent=min_ha2/Areas[i]*100))
+  mutate(mean_area_percent=mean_ha2/(Itcha$total.area[Itcha$BCHab_code==habitat_types[i]])*100,
+         median_area_percent = median_ha2/Itcha$total.area[Itcha$BCHab_code==habitat_types[i]]*100,
+         max_area_percent=max_ha2/Itcha$total.area[Itcha$BCHab_code==habitat_types[i]]*100,
+         min_area_percent=min_ha2/Itcha$total.area[Itcha$BCHab_code==habitat_types[i]]*100,
+         CI_90_percent=CI_90_upper_ha2/Itcha$total.area[Itcha$BCHab_code==habitat_types[i]]*100,
+         CI_95_percent=CI_95_upper_ha2/Itcha$total.area[Itcha$BCHab_code==habitat_types[i]]*100))
          
-  tab2<- as.data.frame(m4 %>% filter(habitat==habitat_types[i]) %>%
-   mutate(CI_90_percent=grp.90.CI/Areas[i]*100,
-          CI_95_percent=grp.95.CI/Areas[i]*100))
-    
-    tab3<-join(tab,tab2)
-
   
   nam1<-paste("area",habitat_types[i],sep=".") #defining the name
 
-  assign(nam1,tab3)
+  assign(nam1,tab)
 }
 
 summary_area_burned<-rbind(area.HEWSR,area.Matrix,area.LEWR,area.LESR)
