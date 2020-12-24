fad7dd411203bd0a44f31b9a6952f397bd97d497
Lochhead <Kyle.Lochhead@gov.bc.ca>
2020-12-23 16:19:53 -0800

    creating reportingConstraints functions

--- R/SpaDES-modules/forestryCLUS/forestryCLUS.R
+++ R/SpaDES-modules/forestryCLUS/forestryCLUS.R
@@ -74,6 +74,7 @@ doEvent.forestryCLUS = function(sim, eventTime, eventType) {
     schedule = {
       sim <- setConstraints(sim)
       sim <- getHarvestQueue(sim) # This returns a candidate set of blocks or pixels that could be harvested
+      sim <- reportConstraints(sim)
       sim <- scheduleEvent(sim, time(sim) + 1, "forestryCLUS", "schedule", 3)
     },
     save = {
@@ -378,8 +379,40 @@ ORDER by block_rank, ", P(sim, "forestryCLUS", "harvestPriority"), "
 }
 
 reportConstraints<- function(sim) {
+  message("....reporting zone constraints")
+  zones<-dbGetQuery(sim$clusdb, "SELECT zone_column FROM zone")
+  for(i in 1:nrow(zones)){ #for each of the specified zone rasters
+    numConstraints<-dbGetQuery(sim$clusdb, paste0("SELECT DISTINCT variable, type FROM zoneConstraints WHERE
+                               zone_column = '",  zones[[1]][i] ,"' AND type IN ('ge', 'le')"))
+    
+    if(nrow(numConstraints) > 0){
+      for(k in 1:nrow(numConstraints)){
+        query_parms<-data.table(dbGetQuery(sim$clusdb, paste0("SELECT t_area, type, zoneid, variable, zone_column, percentage, threshold, multi_condition
+                                                        FROM zoneConstraints WHERE zone_column = '", zones[[1]][i],"' AND variable = '", 
+                                                              numConstraints[[1]][k],"' AND type = '",numConstraints[[2]][k] ,"';")))
+        switch(
+          as.character(query_parms[1, "type"]),
+          ge = {
+            sql<- paste0("INSERT INTO zoneManagement SELECT AVG(case when", numConstraints[[1]][k]  ," > :threshold then 1 else 0 end) as percent, zone1, ", as.integer(time(sim)) ," as timeperiod  from pixels group by zone1")
+            
+            dbBegin(sim$clusdb)
+            rs<-dbSendQuery(sim$clusdb, sql, query_parms[,c("zoneid")])
+            dbClearResult(rs)
+            dbCommit(sim$clusdb)
+          },
+          le = {
+            sql<- paste0("INSERT INTO zoneManagement SELECT AVG(case when", numConstraints[[1]][k]  ," < :threshold then 1 else 0 end) as percent, zone1, ", as.integer(time(sim)) ," as timeperiod  from pixels group by zone1")
+            
+            dbBegin(sim$clusdb)
+            rs<-dbSendQuery(sim$clusdb, sql, query_parms[,c("zoneid", "limits")])
+            dbClearResult(rs)
+            dbCommit(sim$clusdb)
+          }
+        )
+      }
+    }
+  }
   
-  dbGetQuery(sim$clusdb, "SELECT AVG(case when age > 140 then 1 else 0 end) as percent, zone1, 0 as timeperiod  from pixels group by zone1")
   return(invisible(sim))
 }
 
