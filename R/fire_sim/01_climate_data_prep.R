bfc5490eb4c1a372bfa8f32c04f6475010149649
ElizabethKleynhans <Elizabeth.Kleynhans@gov.bc.ca>
2021-01-21 14:47:35 -0800

    minor additions and changes

--- R/fire_sim/01_climate_data_prep.R
+++ R/fire_sim/01_climate_data_prep.R
@@ -76,6 +76,29 @@ fire_igni_bec<- st_read (dsn = conn,
           layer = c ("public", "fire_ignit_by_bec"))
 dbDisconnect (conn)
 
+#getting long lat info
+#geo.prj <- "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0" 
+fire_igni_bec1 <- st_transform(fire_igni_bec, crs = "+proj=longlat +datum=NAD83 / BC Albers +no_defs")
+st_crs(fire_igni_bec1)
+fire_igni_bec2<-as.data.frame(fire_igni_bec1)
+fire_igni_bec2<-fire_igni_bec2 %>% 
+  dplyr::select(ogc_fid: fire_type, size_ha:wkb_geometry)
+# Try find a way to split the data up into 3 colums and the remove the brackets. 
+fire_igni_bec3<- fire_igni_bec2 %>%
+  tidyr::separate(wkb_geometry, into = c("longitude", "latitude")," ")
+fire_igni_bec3$longitude<- gsub(",", "", as.character(fire_igni_bec3$longitude) )
+fire_igni_bec3$longitude<- substring(fire_igni_bec3$longitude, 3)
+fire_igni_bec3$longitude<- as.numeric(fire_igni_bec3$longitude)
+fire_igni_bec3$longitude<- round(fire_igni_bec3$longitude, digits=4)
+fire_igni_bec3$latitude<- gsub(")", "", as.character(fire_igni_bec3$latitude) )
+fire_igni_bec3$latitude<- as.numeric(fire_igni_bec3$latitude)
+fire_igni_bec3$latitude<- round(fire_igni_bec3$latitude, digits=4)
+
+fire_igni_bec_new<-fire_igni_bec %>% dplyr::select(ogc_fid: fire_type, size_ha: wkb_geometry)
+
+fire_igni_bec_new$longitude<-fire_igni_bec3$longitude
+fire_igni_bec_new$latitude<-fire_igni_bec3$latitude
+
 # Now Ill buffer each fire location by 500m and then within each Bec Zone Ill sample locations where fires did not start and combine those locations with locations where the fires did start.
 
 years<-c("2002", "2003", "2004", "2005", "2006", "2007","2008","2009","2010","2011","2012","2013","2014","2015","2016","2017","2018", "2019", "2020")
@@ -84,7 +107,7 @@ filenames<-list()
 
 for (i in 1:length(years)) {
   print(years[i])
-  foo<- fire_igni_bec %>% filter(fire_year==years[i])
+  foo<- fire_igni_bec_new %>% filter(fire_year==years[i])
   foo_ignit_sf<- st_as_sf(foo)
   
   for (j in 1:length(beczone)) {
@@ -119,9 +142,9 @@ for (i in 1:length(years)) {
     samp_joined$fire<-0
     
     #getting long lat info
-    # reproject sp object
-    geo.prj <- "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0" 
-    sample.p.trans <- st_transform(samp_joined, crs = geo.prj)
+    #geo.prj <- "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0" 
+    sample.p.trans <- st_transform(samp_joined, crs = "+proj=longlat +datum=NAD83 / BC Albers +no_defs")
+    #st_crs(sample.p.trans)
     sample.p.trans1<-as.data.frame(sample.p.trans)
     # Try find a way to split the data up into 3 colums and the remove the brackets. 
     samp_joined2<- sample.p.trans1 %>%
@@ -129,16 +152,16 @@ for (i in 1:length(years)) {
     samp_joined2$longitude<- gsub(",", "", as.character(samp_joined2$longitude) )
     samp_joined2$longitude<- substring(samp_joined2$longitude, 3)
     samp_joined2$longitude<- as.numeric(samp_joined2$longitude)
+    samp_joined2$longitude<- round(samp_joined2$longitude, digits=4)
     samp_joined2$latitude<- gsub(")", "", as.character(samp_joined2$latitude) )
     samp_joined2$latitude<- as.numeric(samp_joined2$latitude)
+    samp_joined2$latitude<- round(samp_joined2$latitude, digits=4)
     
     samp_joined$longitude<-samp_joined2$longitude
     samp_joined$latitude<-samp_joined2$latitude
     samp_joined_new<- samp_joined %>% 
       rename(wkb_geometry=geometry) %>%
       dplyr::select(ogc_fid, fire_no, fire_year, ign_date, fire_cause, fire_id, fire_type, latitude, longitude, size_ha, objectid, feature_class_skey, zone, subzone, natural_disturbance,zone_name, wkb_geometry, fire)
-    
-    foo_ignit_small
     
     pnts<- rbind(samp_joined_new, foo_ignit_small)
     
@@ -188,23 +211,10 @@ dbDisconnect (conn)
 # or save it as a shape file
 st_write(samp_locations_sf, dsn = "D:\\Fire\\fire_data\\raw_data\\ClimateBC_Data\\samp_locations_fire.shp")
 
-# when I try to run these locations in climateBC it says there are locations that are beyond the bounds. I think there are locations in the ocean between vancouver island and the mainland and I think there are causing the issue. So ill clip these things out.
-
-forest_tenure<-getSpatialQuery("SELECT tsa_number, tsnmbrdscr, geom FROM public.forest_tenure")
-
-forest_tenure<-st_transform(forest_tenure, 3005)
-#plot(bec[, "zone"]) # check we got the whole province
-
-tsa <-forest_tenure %>% 
-  filter (tsnmbrdscr != 'Queen Charlotte Timber Supply Area') %>%
-  filter(tsnmbrdscr != 'North Island Timber Supply Area') %>%
-  filter(tsnmbrdscr != 'Arrowsmith Timber Supply Area') %>%
-  filter(tsnmbrdscr != 'Pacific Timber Supply Area')
-
-sample_locations<-samp_locations_sf[tsa,] # clipping out the above TSA's in the hope this solves the problem with extracting data from climateBC
+# If an error message appears when you run these csv files in climateBC make sure that when the csv was written an extra column was not added. row.names=FALSE should prevent this.
 
 for (i in 1: length(years)) {
-  dat<- sample_locations %>% filter(fire_year==years[i])
+  dat<- samp_locations_sf %>% filter(fire_year==years[i])
   sample.pts <- data.frame (matrix (ncol = 5, nrow = nrow (dat)))
   colnames (sample.pts) <- c ("ID1","ID2", "lat", "long", "el")
   sample.pts$ID1<- dat$idno
@@ -215,9 +225,124 @@ for (i in 1: length(years)) {
   
   nam1<-paste("sampled.points",years[i], "csv",sep=".")
   the_dir <- "D:\\Fire\\fire_data\\raw_data\\ClimateBC_Data"
-  write.csv(sample.pts, file = paste0(the_dir, "\\", basename(nam1)))
+  write.csv(sample.pts, file = paste0(the_dir, "\\", basename(nam1)), row.names=FALSE)
 }
 
+
+# after these files are written manually extract the monthly climate data for each of the relevant years at each of the fire ignition and sample locations from climate BC (http://climatebc.ca/) and save the files as .csv's. Here I import them again.
+
+
+###############################
+#Import climate data per ignition and sample location
+###############################
+
+file.list1<-list.files("D:\\Fire\\fire_data\\raw_data\\ClimateBC_Data\\output", pattern="sampled.points", all.files=FALSE, full.names=FALSE)
+y1<-gsub(".csv","",file.list1)
+the_dir <- "D:\\Fire\\fire_data\\raw_data\\ClimateBC_Data\\output"
+
+for (i in 1:length(file.list1)){
+  assign(paste0(y1[i]),read.csv (file=paste0(the_dir, "\\", file.list1[i])))
+}
+
+# FOR EACH DATASET CALCULATE THE MONTHLY DROUGHT CODE
+
+#############################################
+#### Equations to calculate drought code ####
+#############################################
+
+days_month<- c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31) # number of days in each month starting in Jan
+#### Daylength adjustment factor (Lf) [Development and Structure of the Canadian Forest Fire Weather Index System pg 15, https://d1ied5g1xfgpx8.cloudfront.net/pdfs/19927.pdf] ####
+# Month <- Lf value
+# LF[1] is the value for Jan
+Lf<-c(-1.6, -1.6, -1.6, 0.9, 3.8, 5.8, 6.4, 5.0, 2.4, 0.4, -1.6, -1.6)
+####
+
+### Calculate drought code for Fire ignition data
+filenames<-list()
+for (i in 1: length(y1)){
+  
+  x<-eval(as.name(y1[i])) %>% 
+    rename(YEAR=ID2) %>%
+    dplyr::select(ID1, YEAR,Latitude, Longitude, Tmax05:Tmax09, Tave05:Tave09, PPT05:PPT09, CMI05:CMI09)
+  
+  x2<- x %>% filter(Tmax05 != -9999) # there are some locations that did not have climate data, probably because they were over the ocean, so Im removing these here.
+  
+  for (j in 5 : 9) {
+    
+    x2$MDC_04<-15
+    
+    Em<- days_month[j]*((0.36*x2[[paste0("Tmax0",j)]])+Lf[j])
+    Em2 <- ifelse(Em<0, 0, Em)
+    DC_half<- x2[[paste0("MDC_0",j-1)]] + (0.25 * Em2)
+    precip<-x2[[paste0("PPT0",j)]]
+    RMeff<-(0.83 * (x2[[paste0("PPT0",j)]]))
+    Qmr<- (800 * exp((-(DC_half))/400)) + (3.937 * RMeff)
+    Qmr2 <- ifelse(Qmr>800, 800, Qmr)
+    MDC_m <- (400 * log(800/Qmr2)) + 0.25*Em2
+    x2[[paste0("MDC_0",j)]] <- (x2[[paste0("MDC_0",j-1)]] + MDC_m)/2
+    x2[[paste0("MDC_0",j)]] <- ifelse(x2[[paste0("MDC_0",j)]] <15, 15, x2[[paste0("MDC_0",j)]])
+  }
+  nam1<-paste("DC.",y1[i],sep="") #defining the name
+  assign(nam1,x2)
+  filenames<-append(filenames,nam1)
+}
+
+# combind all the DC.ignition files together
+mkFrameList <- function(nfiles) {
+  d <- lapply(seq_len(nfiles),function(i) {
+    eval(parse(text=filenames[i]))
+  })
+  do.call(rbind,d)
+}
+
+n<-length(filenames)
+DC.ignitions<-mkFrameList(n) 
+DC.ignitions$ID1<- as.factor(DC.ignitions$ID1)
+
+dim(DC.ignitions) 
+names(DC.ignitions)
+names(samp_locations_sf)
+
+DC.ignitions1<- DC.ignitions %>% rename(idno=ID1,
+                                        fire_year=YEAR) 
+samp_locations_sf$idno <- as.factor(as.character(samp_locations_sf$idno))
+samp_locations_sf$fire_year <- as.numeric(as.character(samp_locations_sf$fire_year))
+
+# Now join DC.ignitions back with the original fire ignition dataset
+ignition_weather<-left_join(DC.ignitions1, samp_locations_sf)
+head(ignition_weather)
+dim(ignition_weather) # should have 57984 rows
+st_crs(ignition_weather)
+ignition_weather_crs <- st_as_sf(ignition_weather)
+crs(ignition_weather_crs)
+ignition_weather_crs<- st_transform(ignition_weather_crs, 3005)
+
+# Check the points line up with BC boundaries!
+ggplot() +
+  geom_sf(data=bc.bnd, col='red') +
+  geom_sf(data=ignition_weather_crs, col='black') # looks good!
+
+st_write(ignition_weather_crs, dsn = "D:\\Fire\\fire_data\\raw_data\\ClimateBC_Data\\DC_data.shp", delete_layer=TRUE)
+
+# commit the shape file to postgres
+# this works for loading the shape file onto Kyles Postgres. Run these sections of code below in R and fill in the details in the script for command prompt. Then run the ogr2ogr script in command prompt to get the table into postgres
+
+# To Kyles Clus
+#host=keyring::key_get('dbhost', keyring = 'postgreSQL')
+#user=keyring::key_get('dbuser', keyring = 'postgreSQL')
+#dbname=keyring::key_get('dbname', keyring = 'postgreSQL')
+#password=keyring::key_get('dbpass', keyring = 'postgreSQL')
+
+# Run this in terminal
+#ogr2ogr -f PostgreSQL PG:"host= user= dbname= password= port=5432" D:\\Fire\\fire_data\\raw_data\\ClimateBC_Data\\DC_data.shp -overwrite -a_srs EPSG:3005 -progress --config PG_USE_COPY YES -nlt PROMOTE_TO_MULTI
+
+# OR my local machine
+
+# ogr2ogr -f "PostgreSQL" PG:"host=localhost user=postgres dbname=postgres password=postgres port=5432" D:\\Fire\\fire_data\\raw_data\\ClimateBC_Data\\dc_data.shp -overwrite -a_srs EPSG:3005 -progress --config PG_USE_COPY YES -nlt PROMOTE_TO_MULTI
+
+#########################################
+#### FINISHED NOW GO TO 01_vri_data_prep####
+#########################################
 
 
   
@@ -225,10 +350,6 @@ for (i in 1: length(years)) {
 
 
 
-
-ggplot() +
-  geom_sf(data=sbs) +
-  geom_sf(data=fire.ignition_sbs) # good they line up
 
 
 # dilemma: should I just make a raster of locations where fires started and did not start and then pull out those locations and get the temperature and vegetation data for the same locations every year or should I filter by year and then randomly select areas taht were not burned in e.g. 2002 and were burned in 2002 i.e. creating a different dataset for each year. I guess i might have problems with temporal autorcorrelation if my locations are the same year after year. 
