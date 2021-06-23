a88f5ecd16d33b1eca712d23adf3b94e45e94156
coraskaien <cora.skaien@gov.bc.ca>
2021-06-23 11:11:54 -0700

    Notes

--- R/fire_sim/03_vri_data_prep.R
+++ R/fire_sim/03_vri_data_prep.R
@@ -18,8 +18,13 @@
 #  Script Contributor: Cora Skaien, Ecological Modeling Specialist, Forest Analysis and Inventory Branch, B.C. Ministry of Forests, Lands, and Natural Resource Operations.
 #=================================
 
+#Overview:
+   # in this file, we acquire the VRI (Vegetation Resources Inventory) for each year from 2002 to 2020.
+   # The final product will be a file with veg data along with ignition data and climate data (vegetation, climate and presence/absence of fire data).
+
 
 #### VEGETATION DATA #### 
+### The below code has already been ran and uploaded to the clus server. Please check the clus serve for relevant files before running again. If present, do not run again as the gdb is very large. Note also that some table names have changed.
 #downloaded for years 2002 to 2019. These are the only years that VRI data exists, there is no earlier data.
 #from https://catalogue.data.gov.bc.ca/dataset/vri-historical-vegetation-resource-inventory-2002-2019-
 # I then extracted this data and uploaded it into my local postgres database by running the command below in terminal. If running it in the R terminal does not work try run it in here: 
@@ -189,8 +194,10 @@ fire_veg_2020$proj_age_1<-NA
 fire_veg_2020$live_stand_volume_125<-NA
 
 ###################################
-#### Check that my solution to calculating volume for 2007 and 2008 is ligitimate. Ill do this by comparing the sum of vol_per_ha_spp1_125 + vol_per_ha_spp2_125 + vol_per_ha_spp3_125 + vol_per_ha_spp4_125 + vol_per_ha_spp5_125 + vol_per_ha_spp6_125 is similar to the values in the column live_stand_volume_125
+#### Check that my solution to calculating volume for 2007 and 2008 is legitimate. Ill do this by comparing the sum of vol_per_ha_spp1_125 + vol_per_ha_spp2_125 + vol_per_ha_spp3_125 + vol_per_ha_spp4_125 + vol_per_ha_spp5_125 + vol_per_ha_spp6_125 is similar to the values in the column live_stand_volume_125
 ###################################
+
+#Must create test file for below to work
 
 names(fire_veg_2009_test)
 plot(fire_veg_2009_test$live_stand_volume_125, fire_veg_2009_test$live_stand_volume_125_try)
@@ -220,6 +227,10 @@ table(fire_veg_data$fire_yr, fire_veg_data$fire_cs)
 # e.g. numbers to check 2002 -> 876 lightning strikes
 #                       2007 -> 22
 #                       2008 -> 29
+
+
+
+## Note that below refers to "fire_ignitions_veg_climate" for the first time. This is not in above.
 
 # write final fire ignitions, weather and vegetation types to postgres
 # save data 
