4c9c9a8311d7120a148644de9c2c73764cfc4ffc
Kleynhans <Elizabeth.Kleynhans@gov.bc.ca>
2022-04-25 11:26:43 -0700

    Simplified some scripts and altered others to include fire regime type instead of natural disturbance type

--- R/fire_sim/01_fire_ignition_data_prep.R
+++ R/fire_sim/01_fire_ignition_data_prep.R
@@ -50,45 +50,19 @@ ignit<-try(
 
 
 head(ignit)
-ignit$FIRE_NUMBER<-as.factor(ignit$FIRE_NUMBER)
 table(ignit$FIRE_YEAR) # yipee Looking at https://www2.gov.bc.ca/gov/content/safety/wildfire-status/about-bcws/wildfire-statistics/wildfire-averages and comparing the values I get to these looks correct with all years present after 2001. 
-table(ignit$FIRE_YEAR, ignit$FIRE_CAUSE) #But notice that there is lots of fire cause data missing. Which is a problem. I've had discussions with the curator on BCGW about this and they think that im loosing this data when I filter. I cant work out how this is happening though. Anyway, because I need these records Im joining this table back to the data we downloaded in 2021. 
-
-ignit2<-frt <- st_read ( dsn = "D:\\Fire\\fire_data\\raw_data\\Historical_Fire_Ignition_point_locations\\PROT_HISTORICAL_INCIDENTS_SP\\H_FIRE_PNT_point.shp", stringsAsFactors = T)
-head(ignit2)
-ignit2$FIRE_NO<-as.factor(ignit2$FIRE_NO)
-
-ignit2<- ignit2 %>%
-  filter(FIRE_YEAR>2001) %>%
-  filter(FIRE_TYPE == "Fire") %>%
-  select(FIRE_NO, FIRE_YEAR, FIRE_CAUSE) %>%
-  rename(FIRE_CAUSE2=FIRE_CAUSE,
-         FIRE_NUMBER=FIRE_NO) 
-
-table(ignit2$FIRE_YEAR, ignit2$FIRE_CAUSE)
-  
-ignit2$FIRE_YEAR2<-ignit2$FIRE_YEAR
-ignit2<-as.data.frame(ignit2)
-ignit2<-ignit2[,c(1:3,5)]
+table(ignit$FIRE_YEAR, ignit$FIRE_CAUSE) 
 
 
-ignition<-ignit %>% left_join(ignit2)
-ignition %>% select(FIRE_YEAR,FIRE_YEAR2, FIRE_NUMBER, FIRE_CAUSE,FIRE_CAUSE2) %>% print(n=100)
-
-# now that the two dataframes are linked Im going to change all the Unknown values in FIRE_CAUSE colum to NA. Then Im going to use the coalesce function to match the columns FIRE_CAUSE and FIRE_CAUSE to pull out any inform to so that I get as much data about fire cause as possible and then change the NA values back to Unknown.
-
-ignition2 <- ignition %>%
-  mutate(FIRE_CAUSE = na_if(FIRE_CAUSE, "Unknown")) %>%
-  mutate(FIRE_CAUSE3 = coalesce(FIRE_CAUSE, FIRE_CAUSE2)) %>%
-  mutate(FIRE_CAUSE3 = coalesce (FIRE_CAUSE3,"Unknown"))
-table(ignition2$FIRE_YEAR, ignition2$FIRE_CAUSE3)
-
-# This is better. Im still missing quite a few fire locations with data especially for the years 2007 - 2009. But with out bugging Devona again this is the best I can do for now.
-
-ignition2 <- st_transform (ignit, 3005)
+ignition <- ignit %>%
+  mutate(FIRE_CAUSE2 = na_if(FIRE_CAUSE, "Unknown")) 
+table(ignition$FIRE_YEAR, ignition$FIRE_CAUSE2)
 
 
-st_write(ignition2, overwrite = TRUE,  dsn="C:\\Work\\caribou\\clus\\R\\fire_sim\\data\\bc_fire_ignition.shp", delete_dsn = TRUE)
+ignition2 <- st_transform (ignition, 3005)
+
+
+#st_write(ignition2, overwrite = TRUE,  dsn="C:\\Work\\caribou\\clus\\R\\fire_sim\\data\\bc_fire_ignition.shp", delete_dsn = TRUE)
 
 ## Load ignition data into postgres (either my local one or Kyles)
 #host=keyring::key_get('dbhost', keyring = 'postgreSQL')
