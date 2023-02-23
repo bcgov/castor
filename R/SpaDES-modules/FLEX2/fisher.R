a5ccb541002265379b7d78fdf3896111095155e2
Sasha Bogdanovic <sasha@rubyind.com>
2023-02-23 00:36:25 -0700

    Check for correct number of arguments

--- R/SpaDES-modules/FLEX2/fisher.R
+++ R/SpaDES-modules/FLEX2/fisher.R
@@ -31,7 +31,7 @@ thisPath <- function() {
 setwd(thisPath())
 
 args = commandArgs(trailingOnly = TRUE)
-if (length(args) != 10) {
+if (length(args) != 9) {
   stop("All arguments must be supplied.\n", call.=FALSE)
 }
 
