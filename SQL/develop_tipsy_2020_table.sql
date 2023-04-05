16021b0fbedcdfc94cbc746484edbd32fbca1973
Lochhead <Kyle.Lochhead@gov.bc.ca>
2023-04-05 16:07:52 -0700

    yield edits for 2020

--- SQL/develop_tipsy_2020_table.sql
+++ SQL/develop_tipsy_2020_table.sql
@@ -1,28 +1,74 @@
 ---TIPSY Development Curves
 ----------------------------
 
+CREATE TABLE IF NOT EXISTS public.tipsy_prov_current_2020
+(
+    ycid integer,
+	stand1 text,
+	stand2 text,
+    age integer,
+    height double precision,
+    vol_gross double precision,
+    vol_conifer double precision,
+    basalarea double precision,
+    qmd double precision,
+    sph double precision,
+    vpt double precision,
+    crownclosure double precision,
+    vol_dec double precision,
+    tvol double precision
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.tipsy_prov_current_2020
+    OWNER to klochhea;
+	
+CREATE TABLE IF NOT EXISTS public.tipsy_prov_2020
+(
+    ycid integer,
+	stand1 text,
+	stand2 text,
+    age integer,
+    height double precision,
+    vol_gross double precision,
+    vol_conifer double precision,
+    basalarea double precision,
+    qmd double precision,
+    sph double precision,
+    vpt double precision,
+    crownclosure double precision,
+    vol_dec double precision,
+    tvol double precision
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.tipsy_prov_2020
+    OWNER to klochhea;	
+
 --remove the output not needed
 alter table tipsy_prov_2020 drop stand1;
 alter table tipsy_prov_2020 drop stand2;
-alter table tipsy_current_prov_2020 drop stand1;
-alter table tipsy_current_prov_2020 drop stand2;
+alter table tipsy_prov_current_2020 drop stand1;
+alter table tipsy_prov_current_2020 drop stand2;
 --add in eca, dec-pcnt
 alter table tipsy_prov_2020 add column eca double precision;
 alter table tipsy_prov_2020 add column dec_pcnt double precision;
-alter table tipsy_current_prov_2020 add column eca double precision;
-alter table tipsy_current_prov_2020 add column dec_pcnt double precision;
+alter table tipsy_prov_current_2020 add column eca double precision;
+alter table tipsy_prov_current_2020 add column dec_pcnt double precision;
 --- Deciduous percentage.*/
 update tipsy_prov_2020 set dec_pcnt = vol_dec/tvol where tvol > 0 and dec_pcnt > 0;
-update tipsy_current_prov_2020 set dec_pcnt = vol_dec/tvol where tvol > 0 and dec_pcnt > 0;
+update tipsy_prov_current_2020 set dec_pcnt = vol_dec/tvol where tvol > 0 and dec_pcnt > 0;
 
 --- format percentages to two decimals;
 update tipsy_prov_2020 set dec_pcnt = ROUND(CAST(dec_pcnt as numeric), 2);
 update tipsy_prov_2020 set height = ROUND(CAST(height as numeric), 2);
 update tipsy_prov_2020 set tvol = ROUND(CAST(tvol as numeric), 2);
 
-update tipsy_current_prov_2020 set dec_pcnt = ROUND(CAST(dec_pcnt as numeric), 2);
-update tipsy_current_prov_2020 set height = ROUND(CAST(height as numeric), 2);
-update tipsy_current_prov_2020 set tvol = ROUND(CAST(tvol as numeric), 2);
+update tipsy_prov_current_2020 set dec_pcnt = ROUND(CAST(dec_pcnt as numeric), 2);
+update tipsy_prov_current_2020 set height = ROUND(CAST(height as numeric), 2);
+update tipsy_prov_current_2020 set tvol = ROUND(CAST(tvol as numeric), 2);
 
 /*STEP 8. Assign ECA. See https://www.for.gov.bc.ca/tasb/legsregs/fpc/FPCGUIDE/wap/WAPGdbk-Web.pdf*/
 ---Table A2.2 Hydrological recovery for fully stocked stands that reach a maximum crown closure of 50%–70%.
@@ -40,29 +86,42 @@ update tipsy_prov_2020 set eca = 0.5 where height >= 5 and height < 7;
 update tipsy_prov_2020 set eca = 0.25 where height >= 7 and height < 9;
 update tipsy_prov_2020 set eca = 0.1 where height >= 9;
 
-update tipsy_current_prov_2020 set eca = 1 where height < 3;
-update tipsy_current_prov_2020 set eca = 0.75 where height >= 3 and height < 5;
-update tipsy_current_prov_2020 set eca = 0.5 where height >= 5 and height < 7;
-update tipsy_current_prov_2020 set eca = 0.25 where height >= 7 and height < 9;
-update tipsy_current_prov_2020 set eca = 0.1 where height >= 9;
+update tipsy_prov_current_2020 set eca = 1 where height < 3;
+update tipsy_prov_current_2020 set eca = 0.75 where height >= 3 and height < 5;
+update tipsy_prov_current_2020 set eca = 0.5 where height >= 5 and height < 7;
+update tipsy_prov_current_2020 set eca = 0.25 where height >= 7 and height < 9;
+update tipsy_prov_current_2020 set eca = 0.1 where height >= 9;
 --- Add in ycgrp********************************************
-alter table tipsy_prov_2020 rename column feature_id to ycid; 
+
 Update tipsy_prov_2020 set tvol = 0 where tvol is NULL;
-alter table tipsy_prov_2020 rename column crown_closure to crownclosure; 
-alter table tipsy_prov_2020 rename column basal_area to basalarea; 
+Update tipsy_prov_current_2020 set tvol = 0 where tvol is NULL;
 
-alter table tipsy_current_prov_2020 rename column feature_id to ycid; 
-update tipsy_current_prov_2020 set ycid = ycid*-1;
 
-CREATE INDEX IF NOT EXISTS idx_tipsy_current_prov_2020_ycid
-    ON public.tipsy_current_prov_2020 USING btree
+CREATE INDEX IF NOT EXISTS idx_tipsy_prov_2020_ycid
+    ON public.tipsy_prov_2020 USING btree
     (ycid ASC NULLS LAST)
     TABLESPACE pg_default;
+
+select min(ycid) from tipsy_prov_2020; ---1810868 is the max ycid
+select count(*) from tipsy_prov_current_2020;
+
+alter table tipsy_prov_current_2020 add column ycid2 integer;
+update tipsy_prov_current_2020 c
+ set ycid2 = c2.seqnum
+   from (select ycid, row_number() over (ORDER BY ycid) as seqnum
+          from tipsy_prov_current_2020 group by ycid order by seqnum
+         ) c2
+    where c2.ycid = c.ycid;
 	
-Update tipsy_current_prov_2020 set tvol = 0 where tvol is NULL;
-alter table tipsy_current_prov_2020 rename column crown_closure to crownclosure; 
-alter table tipsy_current_prov_2020 rename column basal_area to basalarea; 
+	
+alter table tipsy_prov_current_2020 rename column ycid to feature_id;
+alter table tipsy_prov_current_2020 rename column ycid2 to ycid;
 
+update tipsy_prov_current_2020 set ycid = ycid*-1;
+select * from tipsy_prov_current_2020 where feature_id = 17850206;
+	
+vacuum tipsy_prov_current_2020;
+select * from veg_comp_lyr_r1_poly2021 where feature_id = 17850206;
 
-
+create table tipsy_prov_current_2020_vat as (select distinct(ycid), feature_id from tipsy_prov_current_2020);
 
