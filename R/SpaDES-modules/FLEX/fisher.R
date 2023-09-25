bdd75a8ccd4a92540eb020b1345a04ed8eab18ec
Sasha Bogdanovic <sasha@rubyind.com>
2023-09-25 11:34:07 -0600

    Changes to how simulations are run in fisher.R shell script

--- R/SpaDES-modules/FLEX/fisher.R
+++ R/SpaDES-modules/FLEX/fisher.R
@@ -36,7 +36,7 @@ if (length(args) != 13) {
 }
 
 library (SpaDES.core)
-library (SpaDES.tools)
+library(SpaDES.experiment)
 library (data.table)
 library (terra)
 library (keyring)
@@ -45,11 +45,11 @@ library (here)
 library (stringr)
 library (truncnorm)
 library (RANN)
-# library(future)
-# library(future.callr)
+library(sampling)
+library(BalancedSampling)
+library(ggplot2)
 library(parallel)
-
-# plan(multisession)
+library(raster)
 
 times <- as.numeric(args[1])
 female_max_age <- as.numeric(args[2])
@@ -65,90 +65,43 @@ d2_target <- as.numeric(args[11])
 initial_fisher_pop <- as.numeric(args[12])
 appx <- as.numeric(args[13])
 
-run_iteration <- function(
-    iteration,
-    times,
-    female_max_age,
-    den_target,
-    rest_target,
-    move_target,
-    reproductive_age,
-    sex_ratio,
-    female_dispersal,
-    time_interval,
-    burn_in_length,
-    d2_target,
-    initial_fisher_pop
-) {
-  # future({
-    moduleDir <- file.path(paste0(here::here(), "/R/SpaDES-modules"))
-    inputDir <- file.path(paste0(here::here(), "/R/scenarios/fisher/inputs")) %>% reproducible::checkPath (create = TRUE)
-    outputDir <- file.path(paste0(here::here(), "/R/scenarios/fisher/outputs/", iteration)) %>% reproducible::checkPath (create = TRUE)
-    cacheDir <- file.path(paste0(here::here(), "/R/scenarios/fisher"))
-    
-    times <- list (start = 0, end = times)
-    
-    parameters <- list(
-      FLEX = list(
-        female_max_age = female_max_age,
-        den_target = den_target,
-        rest_target = rest_target,
-        move_target = move_target,
-        reproductive_age = reproductive_age,
-        sex_ratio = sex_ratio,
-        female_dispersal = female_dispersal,  # ha; radius = 500 pixels = 50km = 7850km2 area
-        timeInterval = time_interval, # should be consistent with the time interval used to model habitat
-        burnInLength = burn_in_length, 
-        d2_target = d2_target,
-        initialFisherPop = initial_fisher_pop,
-        # e.g., growingstockLCUS periodLength
-        iterations = 1, # not currently implemented
-        rasterHabitat = paste0(here::here(), "/R/scenarios/fisher/inputs/scenario.tif")
-      )
-    )
-    
-    scenario = data.table (name = "test",
-                           description = "Testing fisher ABM.")
-    
-    modules <- list ("FLEX")
-    
-    objects <- list (scenario = scenario)
-    inputs <- list ()
-    outputs <- data.frame (objectName = c())
-    
-    paths <- list(cachePath = cacheDir,
-                  modulePath = moduleDir,
-                  inputPath = inputDir,
-                  outputPath = outputDir)
-    
-    mySim <- simInit(times = times,
-                     params = parameters,
-                     modules = modules,
-                     objects = objects,
-                     paths = paths)
-    
-    fisherSimOut <- spades(mySim)
-    return(NULL)
-  # })
-}
-
 cores <- parallel::detectCores()
 
-parallel::mclapply(
-  X = 1:appx,
-  FUN = run_iteration,
-  mc.cores = cores,
-  times,
-  female_max_age,
-  den_target,
-  rest_target,
-  move_target,
-  reproductive_age,
-  sex_ratio,
-  female_dispersal,
-  time_interval,
-  burn_in_length,
-  d2_target,
-  initial_fisher_pop
+moduleDir <- file.path(paste0(here::here(), "/R/SpaDES-modules"))
+paths <- list(
+  modulePath = paste0(here::here(),"/R/SpaDES-modules"),
+  outputPath = paste0(here::here(),"/R/scenarios/fisher/outputs")
 )
 
+times <- list (start = 0, end = times - 1)
+
+parameters <- list(
+  FLEX = list(
+    female_max_age = female_max_age,
+    den_target = den_target, 
+    rest_target = rest_target,
+    move_target = move_target,
+    reproductive_age = reproductive_age,
+    sex_ratio = sex_ratio,
+    female_dispersal = female_dispersal,  # ha; radius = 500 pixels = 50km = 7850km2 area
+    timeInterval = time_interval, # should be consistent with the time interval used to model habitat
+    burnInLength = burn_in_length, 
+    d2_target = d2_target,
+    initialFisherPop = initial_fisher_pop,
+    rasterHabitat = here::here("R/scenarios/fisher/inputs/scenario.tif")
+  )
+)
+
+modules <- list ("FLEX")
+
+mySim <- simInit(
+  times = times, 
+  params = parameters, 
+  modules = modules,
+  objects = list(scenario = data.table(name = "test")),
+  paths = paths
+)
+
+raster::beginCluster(cores)
+experiment(mySim, replicates = cores)
+raster::endCluster()
