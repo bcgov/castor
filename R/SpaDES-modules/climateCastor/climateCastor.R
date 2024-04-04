26fb1cb4ed5c5bcbdc1ce4bf97000aa30d73b097
Kleynhans <Elizabeth.Kleynhans@gov.bc.ca>
2024-04-04 07:45:51 -0700

    minor bug removed

--- R/SpaDES-modules/climateCastor/climateCastor.R
+++ R/SpaDES-modules/climateCastor/climateCastor.R
@@ -231,7 +231,7 @@ getClimateDataForProvince <- function(sim) {
   dbClearResult(rs)
   dbCommit(sim$castordb)
   
-  rm(ds_out,ds_out_summary,xyzTable, x1, x2, x3, x4, x5, x6, x7, x8,x9, x10, x11, x12, x13, x14, x15, inputs)
+  rm(ds_out,ds_out_summary, x1, x2, x3, x4, x5, x6, x7, x8,x9, x10, x11, x12, x13, x14, x15, inputs)
   gc()
   
   } else {
